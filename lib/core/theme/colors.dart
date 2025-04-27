29aab908e28fcbffdb8d3dd990770b6552613f12
@hexfa <amir.dehdarian2009@gmail.com>
2025-04-27 20:24:10 +0300

    Add colors.dart

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+/// Light Theme Colors
+const Color primaryColor = Colors.blueAccent;
+const Color onPrimaryColor = Colors.white;
+const Color card = Colors.white;
+const Color onCard = Colors.black87;
+const Color backgroundColor = Colors.white;
+const Color onBackground = Colors.black;
+const Color secondaryColor = Color(0xFF388E3C); // Green color
+const Color onSecondaryColor = Colors.white;
+const Color tertiary = Colors.grey;
+final Color tertiaryContainer = Colors.grey[200]!;
+final Color secondaryContainerColor = Colors.green[50]!;
+const Color onSecondaryContainerColor = Colors.green;
+const Color error = Color(0xffff6767);
+const Color onError = Colors.white;
+
+/// Dark Theme Colors
+const Color darkBackgroundColor = Color(0xFF2f2f2f);
+const Color darkPrimaryColor = Color(0xff566baf); // Dark blue
+const Color darkOnPrimaryColor = Colors.white;
+const Color darkCard = Colors.black12;
+const Color darkOnCard = Colors.white70;
+const Color darkOnBackground = Colors.white;
+const Color darkSecondaryColor = Color(0xFF4CAF50); // Dark Green
+const Color darkOnSecondaryColor = Colors.white;
+const Color darkTertiary = Colors.grey;
+final Color darkTertiaryContainer = Colors.grey[800]!;
+const Color darkError = Color(0xffff6767);
+const Color darkOnError = Colors.black;
