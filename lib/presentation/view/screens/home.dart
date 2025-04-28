4af1bbe05e70eab1ab62bddbd089abb122df3b92
@hexfa <amir.dehdarian2009@gmail.com>
2025-04-28 18:56:04 +0300

    Clean code

--- lib/presentation/view/screens/home.dart
+++ lib/presentation/view/screens/home.dart
@@ -6,13 +6,33 @@ class HomeScreen extends BaseStatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String balance = '\$5,000';
+    String balance = '\$2,455';
+    String income = '\$1,500';
+    String expense = '\$343,0';
     return Scaffold(
       backgroundColor: theme(context).colorScheme.surface,
       appBar: _homeAbbBar(),
       body: Center(
         child: Column(
-          children: [Text(balance, style: theme(context).textTheme.titleLarge)],
+          children: [
+            Text(balance, style: theme(context).textTheme.titleLarge),
+            Row(
+              children: [
+                _buildIncomeExpenseTile(
+                  title: 'Income',
+                  subtitle: income,
+                  icon: Icons.arrow_upward,
+                  theme: theme(context),
+                ),
+                _buildIncomeExpenseTile(
+                  title: 'Expense',
+                  subtitle: expense,
+                  icon: Icons.arrow_downward,
+                  theme: theme(context),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
@@ -49,5 +69,20 @@ PreferredSizeWidget _homeAbbBar() {
         },
       ),
     ],
+  );
+}
+
+Widget _buildIncomeExpenseTile({
+  required String title,
+  required String subtitle,
+  required IconData icon,
+  required ThemeData theme,
+}) {
+  return Expanded(
+    child: ListTile(
+      leading: Icon(icon, color: theme.colorScheme.secondary),
+      title: Text(title, style: theme.textTheme.titleSmall),
+      subtitle: Text(subtitle, style: theme.textTheme.titleMedium),
+    ),
   );
 }
