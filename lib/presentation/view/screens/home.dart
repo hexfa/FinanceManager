64b758e369241bfb6632051b6add5af6be836c36
@hexfa <amir.dehdarian2009@gmail.com>
2025-04-28 18:11:52 +0300

    Add title in home appbar

--- lib/presentation/view/screens/home.dart
+++ lib/presentation/view/screens/home.dart
@@ -1,10 +1,24 @@
+import 'package:finance_manager/presentation/base/base_stateless_widget.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends BaseStatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text('Home');
+    return Scaffold(appBar: _homeAbbBar());
   }
+}
+
+PreferredSizeWidget _homeAbbBar() {
+  return AppBar(
+    title: Row(
+      children: [
+        Icon(Icons.currency_exchange),
+        SizedBox(width: 8),
+        Text('Balance'),
+      ],
+    ),
+    centerTitle: false,
+  );
 }
