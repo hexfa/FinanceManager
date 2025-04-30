89fd034797fe52c7c6dccf9d2364831cbc0909d8
@hexfa <amir.dehdarian2009@gmail.com>
2025-04-30 18:42:56 +0300

    Improve PieDataList UI

--- lib/presentation/view/screens/home/PieDataList.dart
+++ lib/presentation/view/screens/home/PieDataList.dart
@@ -1,7 +1,8 @@
 import 'package:finance_manager/data/models/PieData.dart';
+import 'package:finance_manager/presentation/base/base_stateless_widget.dart';
 import 'package:flutter/material.dart';
 
-class PieDataList extends StatelessWidget {
+class PieDataList extends BaseStatelessWidget {
   final List<PieData> data;
 
   const PieDataList({super.key, required this.data});
@@ -15,18 +16,23 @@ class PieDataList extends StatelessWidget {
       itemBuilder: (context, index) {
         final item = data[index];
         return Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 8),
           child: Row(
             children: [
               CircleAvatar(radius: 6, backgroundColor: item.color),
               const SizedBox(width: 8),
               Expanded(
-                child: Text(item.title, style: const TextStyle(fontSize: 16)),
+                child: Text(
+                  item.title,
+                  style: theme(context).textTheme.bodyMedium?.copyWith(
+                    color: theme(context).colorScheme.onSurface,
+                  ),
+                ),
               ),
               Text(
                 '${item.percentage}%',
-                style: const TextStyle(
-                  fontSize: 16,
+                style: theme(context).textTheme.bodyMedium?.copyWith(
+                  color: theme(context).colorScheme.onSurface,
                   fontWeight: FontWeight.bold,
                 ),
               ),
