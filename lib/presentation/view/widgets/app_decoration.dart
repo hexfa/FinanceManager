9100b3fac55ad27a75173ba9bc5a7c6bab4f5a3e
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-03 11:58:40 +0300

    Add customInputDecoration

---
+++ lib/presentation/view/widgets/app_decoration.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+InputDecoration customInputDecoration({
+  required BuildContext context,
+  String? label,
+  Widget? prefixIcon,
+}) {
+  final theme = Theme.of(context);
+  return InputDecoration(
+    filled: true,
+    fillColor: theme.colorScheme.surface,
+    labelText: label,
+    floatingLabelBehavior: FloatingLabelBehavior.auto,
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8),
+    ),
+    enabledBorder: OutlineInputBorder(
+      borderSide: BorderSide(color: theme.colorScheme.tertiary),
+      borderRadius: BorderRadius.circular(8),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderSide: BorderSide(color: theme.colorScheme.primary),
+      borderRadius: BorderRadius.circular(8),
+    ),
+    prefixIcon: prefixIcon,
+  );
+}
