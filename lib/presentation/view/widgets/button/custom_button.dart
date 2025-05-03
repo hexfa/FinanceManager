bc786ad9414b04b4581ea8c11b7d9abe2aca145a
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-03 15:09:21 +0300

    Add custom_button.dart

---
+++ lib/presentation/view/widgets/button/custom_button.dart
@@ -0,0 +1,32 @@
+import 'package:finance_manager/presentation/view/base/base_stateless_widget.dart';
+import 'package:flutter/material.dart';
+
+class CustomButton extends BaseStatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const CustomButton({super.key, required this.text, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: theme(context).colorScheme.primary,
+          padding: const EdgeInsets.symmetric(vertical: 20),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        ),
+        onPressed: onPressed,
+        child: Text(
+          text,
+          style: theme(context).textTheme.labelLarge?.copyWith(
+            color: theme(context).colorScheme.onPrimary,
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
