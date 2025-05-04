64b86b21b60c5b7daecb6554b8203a5b77f43644
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-04 17:18:28 +0300

    Add transaction_state.dart

---
+++ lib/presentation/bloc/transaction/transaction_state.dart
@@ -0,0 +1,4 @@
+import 'package:finance_manager/core/utils/transaction_type.dart';
+import 'package:finance_manager/presentation/bloc/base/base_bloc_state.dart';
+
+abstract class AddTransactionState extends BaseBlocState {}
