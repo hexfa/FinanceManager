ec5dbe8f0f53ba2feb39c0510f1159e4426331b3
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-04 17:39:18 +0300

    Update transaction_state.dart

--- lib/presentation/bloc/transaction/transaction_state.dart
+++ lib/presentation/bloc/transaction/transaction_state.dart
@@ -1,6 +1,37 @@
 import 'package:finance_manager/core/utils/transaction_type.dart';
 import 'package:finance_manager/presentation/bloc/base/base_bloc_state.dart';
 
-abstract class TransactionState extends BaseBlocState {}
+class TransactionState extends BaseBlocState {
+  final String title;
+  final String amount;
+  final String category;
+  final DateTime? date;
+  final TransactionType? type;
 
-final class TransactionInitial extends TransactionState {}
+  const TransactionState({
+    this.title = '',
+    this.amount = '',
+    this.category = '',
+    this.date,
+    this.type,
+  });
+
+  TransactionState copyWith({
+    String? title,
+    String? amount,
+    String? category,
+    DateTime? date,
+    TransactionType? type,
+  }) {
+    return TransactionState(
+      title: title ?? this.title,
+      amount: amount ?? this.amount,
+      category: category ?? this.category,
+      date: date ?? this.date,
+      type: type ?? this.type,
+    );
+  }
+
+  @override
+  List<Object?> get props => [title, amount, category, date, type];
+}
