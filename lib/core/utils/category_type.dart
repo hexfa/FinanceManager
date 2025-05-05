d40909bd32074d791f6094a61be8bb3f131152a5
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-05 21:08:42 +0300

    Update category_type.dart

--- lib/core/utils/category_type.dart
+++ lib/core/utils/category_type.dart
@@ -1,1 +1,10 @@
 enum CategoryType { business, food, education, sport, other }
+
+extension CategoryTypeMapper on String {
+  CategoryType toCategoryType() {
+    return CategoryType.values.firstWhere(
+      (e) => e.name == toLowerCase(),
+      orElse: () => CategoryType.other,
+    );
+  }
+}
