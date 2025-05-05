3aa3167f39cde70b3907b9423433dd682586d9bc
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-05 17:18:49 +0300

    Add transaction_adapter.dart

---
+++ lib/domain/entities/adapters/transaction_adapter.dart
@@ -0,0 +1,29 @@
+import 'package:finance_manager/core/utils/category_type.dart';
+import 'package:finance_manager/core/utils/transaction_type.dart';
+import 'package:finance_manager/data/models/transaction.dart';
+import 'package:hive/hive.dart';
+
+class TransactionAdapter extends TypeAdapter<Transaction> {
+  @override
+  final int typeId = 2;
+
+  @override
+  Transaction read(BinaryReader reader) {
+    return Transaction(
+      title: reader.readString(),
+      amount: reader.readDouble(),
+      type: reader.read() as TransactionType,
+      category: reader.read() as CategoryType,
+      date: DateTime.fromMillisecondsSinceEpoch(reader.readInt()),
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Transaction obj) {
+    writer.writeString(obj.title);
+    writer.writeDouble(obj.amount);
+    writer.write(obj.type);
+    writer.write(obj.category);
+    writer.writeInt(obj.date.millisecondsSinceEpoch);
+  }
+}
