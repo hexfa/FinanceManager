d3f219f2244e940820c576526ac509423aed46fa
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-06 20:10:45 +0300

    Resolve bug related transaction_adapter.dart

--- lib/domain/entities/adapters/transaction_adapter.dart
+++ lib/domain/entities/adapters/transaction_adapter.dart
@@ -1,15 +1,15 @@
 import 'package:finance_manager/core/utils/category_type.dart';
 import 'package:finance_manager/core/utils/transaction_type.dart';
-import 'package:finance_manager/data/models/transaction.dart';
+import 'package:finance_manager/domain/entities/transaction_entity.dart';
 import 'package:hive/hive.dart';
 
-class TransactionAdapter extends TypeAdapter<Transaction> {
+class TransactionAdapter extends TypeAdapter<TransactionEntity> {
   @override
   final int typeId = 2;
 
   @override
-  Transaction read(BinaryReader reader) {
-    return Transaction(
+  TransactionEntity read(BinaryReader reader) {
+    return TransactionEntity(
       id: reader.readString(),
       title: reader.readString(),
       amount: reader.readDouble(),
@@ -20,7 +20,8 @@ class TransactionAdapter extends TypeAdapter<Transaction> {
   }
 
   @override
-  void write(BinaryWriter writer, Transaction obj) {
+  void write(BinaryWriter writer, TransactionEntity obj) {
+    writer.writeString(obj.id);
     writer.writeString(obj.title);
     writer.writeDouble(obj.amount);
     writer.write(obj.type);
