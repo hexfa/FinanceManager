1fb08ebdf85e393482b6711d06b5f107c9474cf2
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-06 19:52:39 +0300

    Update transaction_repository_imp.dart

--- lib/data/repositories/transaction_repository_imp.dart
+++ lib/data/repositories/transaction_repository_imp.dart
@@ -1,3 +1,4 @@
+import 'package:finance_manager/core/extension/entity_extension.dart';
 import 'package:finance_manager/data/mapper/transaction_mapper.dart';
 import 'package:finance_manager/data/models/transaction.dart';
 import 'package:finance_manager/domain/entities/transaction_entity.dart';
@@ -8,7 +9,15 @@ class TransactionRepositoryImpl implements TransactionRepository {
   final _db = Hive.box<TransactionEntity>('transactions');
 
   @override
+  Future<List<Transaction>> getAllTransactions() async {
+    final transactions = _db.values.toList();
+    print('------------------- getAllTransactions ${transactions.length}');
+    return transactions.map((entity) => entity.toModel()).toList();
+  }
+
+  @override
   Future<void> createTransaction({required Transaction transaction}) async {
+    // print('-------------------repo');
     _db.add(transaction.toDb());
   }
 }
