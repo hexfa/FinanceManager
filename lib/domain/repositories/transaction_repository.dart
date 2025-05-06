3017e6ff10eb70ccb47e7a93549ed50ccba48ee3
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-06 19:52:15 +0300

    Update transaction_repository.dart

--- lib/domain/repositories/transaction_repository.dart
+++ lib/domain/repositories/transaction_repository.dart
@@ -2,4 +2,6 @@ import 'package:finance_manager/data/models/transaction.dart';
 
 abstract class TransactionRepository {
   Future<void> createTransaction({required Transaction transaction});
+
+  Future<List<Transaction>> getAllTransactions();
 }
