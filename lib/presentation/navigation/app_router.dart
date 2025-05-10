0a0b3582e687e9be4b8857df39ca83c5d665e25d
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-10 18:21:20 +0300

    Handle page not found

--- lib/presentation/navigation/app_router.dart
+++ lib/presentation/navigation/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:finance_manager/presentation/navigation/route_path.dart';
 import 'package:finance_manager/presentation/view/screens/home/home_screen.dart';
 import 'package:finance_manager/presentation/view/screens/transaction/add_transaction_screen.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class AppRouter {
@@ -16,5 +17,8 @@ class AppRouter {
         builder: (context, state) => const AddTransaction(),
       ),
     ],
+    errorBuilder:
+        (context, state) =>
+            const Scaffold(body: Center(child: Text('404 - Page not found'))),
   );
 }
