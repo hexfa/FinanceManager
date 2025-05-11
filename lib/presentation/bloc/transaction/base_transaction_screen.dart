4f4ed608d72f69820a3995be00484748073e7460
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-11 17:16:29 +0300

    Add base_transaction_screen.dart

---
+++ lib/presentation/bloc/transaction/base_transaction_screen.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:finance_manager/presentation/bloc/transaction/transaction_cubit.dart';
+import 'package:finance_manager/presentation/bloc/transaction/transaction_state.dart';
+import 'package:finance_manager/presentation/view/widgets/appbar/custom_app_bar.dart';
+import 'package:finance_manager/presentation/view/widgets/button/custom_button.dart';
+import 'package:finance_manager/presentation/view/widgets/choice_chip/custom_type_selector.dart';
+import 'package:finance_manager/presentation/view/widgets/datetime/custom_date_picker.dart';
+import 'package:finance_manager/presentation/view/widgets/dropdown/custom_drop_down.dart';
+import 'package:finance_manager/presentation/view/widgets/textfield/custom_text_field.dart';
+
+abstract class BaseTransactionScreen extends StatefulWidget {
+  const BaseTransactionScreen({super.key});
+
+  String get appBarTitle;
+
+  String get actionButtonText;
+
+  VoidCallback get onActionPressed;
+}
+
+abstract class BaseTransactionScreenState<T extends BaseTransactionScreen>
+    extends State<T> {
+  late TextEditingController titleController;
+  late TextEditingController amountController;
+
+  @override
+  void initState() {
+    super.initState();
+    titleController = TextEditingController();
+    amountController = TextEditingController();
+
+    titleController.addListener(() {
+      context.read<TransactionCubit>().updateTitle(titleController.text);
+    });
+    amountController.addListener(() {
+      context.read<TransactionCubit>().updateAmount(amountController.text);
+    });
+  }
+
+  @override
+  void dispose() {
+    titleController.dispose();
+    amountController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(title: widget.appBarTitle),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        child: Column(
+          children: [
+            CustomTextField(label: 'Title', controller: titleController),
+            const SizedBox(height: 16),
+            CustomTextField(
+              isCurrency: true,
+              label: 'Amount',
+              controller: amountController,
+            ),
+            const SizedBox(height: 16),
+            CustomDropdown(
+              label: 'Category',
+              items: ['Work', 'Sport', 'Education', 'Other'],
+              onChanged: (value) {
+                context.read<TransactionCubit>().updateCategory(value ?? '');
+              },
+            ),
+            const SizedBox(height: 16),
+            DateTimePickerContainer(
+              onDateTimeSelected: (selectedDateTime) {
+                context.read<TransactionCubit>().updateDate(selectedDateTime);
+              },
+            ),
+            const SizedBox(height: 16),
+            BlocBuilder<TransactionCubit, TransactionState>(
+              builder: (context, state) {
+                return CustomTypeSelector(
+                  selectedType: state.type,
+                  onChanged: (type) {
+                    context.read<TransactionCubit>().updateType(type);
+                  },
+                );
+              },
+            ),
+            const SizedBox(height: 16),
+            BlocBuilder<TransactionCubit, TransactionState>(
+              builder: (context, state) {
+                final isFormValid =
+                    state.title.isNotEmpty &&
+                    state.amount.isNotEmpty &&
+                    state.category.isNotEmpty;
+
+                return CustomButton(
+                  text: widget.actionButtonText,
+                  onPressed: isFormValid ? widget.onActionPressed : null,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
