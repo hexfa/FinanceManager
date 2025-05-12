d474c82e8b24b56b0248599177f12386fa56d252
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-12 20:12:32 +0300

    Add update screen to route path

--- lib/presentation/navigation/route_path.dart
+++ lib/presentation/navigation/route_path.dart
@@ -2,4 +2,5 @@ class RoutePath {
   static const String homeRoute = '/home';
   static const String createTransactionRoute = '/createTransaction';
   static const String transactionDetailRoute = '/transactionDetail';
+  static const String updateTransactionRoute = '/updateTransaction';
 }
