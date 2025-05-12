ec165a586e6bf2cb218426895dbeb89c7dc5d757
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-12 18:46:43 +0300

    Add detail_transaction_screen.dart

---
+++ lib/presentation/view/screens/transaction/detail/detail_transaction_screen.dart
@@ -0,0 +1,11 @@
+import 'package:finance_manager/presentation/view/base/base_stateless_widget.dart';
+import 'package:flutter/material.dart';
+
+class DetailTransactionScreen extends BaseStatelessWidget {
+  const DetailTransactionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
