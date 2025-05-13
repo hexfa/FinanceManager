a7718056f04e3a7f69eac6ca3aee7e8b018d059e
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-13 20:35:44 +0300

    Add delete transaction

--- lib/presentation/view/screens/transaction/detail/detail_transaction_screen.dart
+++ lib/presentation/view/screens/transaction/detail/detail_transaction_screen.dart
@@ -1,10 +1,12 @@
 import 'package:finance_manager/core/utils/convert_string.dart';
 import 'package:finance_manager/data/models/app_bar_menu.dart';
 import 'package:finance_manager/data/models/transaction.dart';
+import 'package:finance_manager/presentation/bloc/transaction/transaction_cubit.dart';
 import 'package:finance_manager/presentation/navigation/route_path.dart';
 import 'package:finance_manager/presentation/view/base/base_stateless_widget.dart';
 import 'package:finance_manager/presentation/view/widgets/appbar/custom_app_bar.dart';
 import 'package:finance_manager/presentation/view/widgets/appbar/custom_app_bar_menu.dart';
+import 'package:finance_manager/presentation/view/widgets/dialog/custom_confirm_dialog.dart';
 import 'package:flutter/material.dart';
 
 class DetailTransactionScreen extends BaseStatelessWidget {
@@ -15,7 +17,7 @@ class DetailTransactionScreen extends BaseStatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: _appBar(context),
+      appBar: _appBar(context, getBloc<TransactionCubit>(context)),
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
@@ -37,14 +39,14 @@ class DetailTransactionScreen extends BaseStatelessWidget {
             SizedBox(height: 4),
             Text(ConvertString.formatDate(transaction.date)),
             SizedBox(height: 16),
-            Text(transaction.type.name)
+            Text(transaction.type.name),
           ],
         ),
       ),
     );
   }
 
-  CustomAppBar _appBar(BuildContext context) {
+  CustomAppBar _appBar(BuildContext context, TransactionCubit cubit) {
     return CustomAppBar(
       title: localization(context).detailTransaction,
       actions: [
@@ -64,7 +66,24 @@ class DetailTransactionScreen extends BaseStatelessWidget {
             AppBarMenu(
               title: localization(context).delete,
               icon: Icons.delete_outline,
-              onTap: () {},
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return CustomConfirmDialog(
+                      title: 'Delete Transaction',
+                      content:
+                          'Are you sure you want to delete this transaction?',
+                      confirmText: 'Delete',
+                      cancelText: 'Cancel',
+                      onConfirm: () async {
+                        await cubit.deleteTransaction(transaction.id ?? 0);
+                        Navigator.of(context).pop();
+                      },
+                    );
+                  },
+                );
+              },
             ),
           ],
         ),
