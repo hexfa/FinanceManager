6fda5971cc2762f723985189045a4590193ca85d
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-14 15:24:11 +0300

    Clean code

--- lib/data/repositories/transaction_repository_imp.dart
+++ lib/data/repositories/transaction_repository_imp.dart
@@ -1,4 +1,5 @@
-import 'package:finance_manager/data/mapper/transaction_mapper.dart';
+import 'package:finance_manager/core/extension/extension.dart';
+import 'package:finance_manager/data/mapper/mapper.dart';
 import 'package:finance_manager/data/models/transaction.dart';
 import 'package:finance_manager/domain/entities/transaction_entity.dart';
 import 'package:finance_manager/domain/repositories/transaction_repository.dart';
@@ -9,8 +10,7 @@ class TransactionRepositoryImpl implements TransactionRepository {
 
   @override
   Future<List<Transaction>> getAllTransactions() async {
-    final transactions = _db.values.toList();
-    return transactions.map((entity) => entity.toModel()).toList();
+    return TransactionMapper.toModelList(_db.values.toList());
   }
 
   @override
