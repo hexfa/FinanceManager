255f6acf478f95206b834fee57b74e41817c1d2f
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-15 17:22:23 +0300

    Verify toEntity conversion from Transaction to TransactionEntity

--- test/core/extension/extension_test.dart
+++ test/core/extension/extension_test.dart
@@ -1,6 +1,7 @@
 import 'package:finance_manager/core/extension/extension.dart';
 import 'package:finance_manager/core/utils/category_type.dart';
 import 'package:finance_manager/core/utils/transaction_type.dart';
+import 'package:finance_manager/data/models/transaction.dart';
 import 'package:finance_manager/domain/entities/transaction_entity.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -22,5 +23,24 @@ void main() {
     expect(model.type, entity.type);
     expect(model.category, entity.category);
     expect(model.date, entity.date);
+  });
+
+  test('Transaction toEntity returns correct TransactionEntity', () {
+    final model = Transaction(
+      id: 1,
+      title: 'Lunch',
+      amount: 50.0,
+      type: TransactionType.expense,
+      category: CategoryType.business,
+      date: DateTime(2024, 1, 1),
+    );
+
+    final entity = model.toEntity();
+
+    expect(entity.title, model.title);
+    expect(entity.amount, model.amount);
+    expect(entity.type, model.type);
+    expect(entity.category, model.category);
+    expect(entity.date, model.date);
   });
 }
