42d1515badc598d82570298bb7933681794a786e
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-15 16:49:49 +0300

    Add unit test for converting TransactionEntity list to model list

--- test/data/mapper/mapper_test.dart
+++ test/data/mapper/mapper_test.dart
@@ -1,4 +1,27 @@
+import 'package:finance_manager/core/utils/category_type.dart';
+import 'package:finance_manager/data/mapper/mapper.dart';
+import 'package:finance_manager/domain/entities/transaction_entity.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:finance_manager/core/utils/transaction_type.dart';
 
 void main() {
+  test(
+    'TransactionMapper converts TransactionEntity list to model list correctly',
+    () {
+      final entities = [
+        TransactionEntity(
+          title: 'Test',
+          amount: 100,
+          category: CategoryType.business,
+          date: DateTime.now(),
+          type: TransactionType.income,
+        ),
+      ];
 
+      final models = TransactionMapper.toModelList(entities);
+
+      expect(models.length, entities.length);
+      expect(models.first.title, entities.first.title);
+    },
+  );
 }
