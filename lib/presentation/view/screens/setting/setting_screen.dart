80cf884fd0b6c02861c0b1409fa0c0e7525a2779
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-17 18:34:08 +0300

    Add theme toggle

--- lib/presentation/view/screens/setting/setting_screen.dart
+++ lib/presentation/view/screens/setting/setting_screen.dart
@@ -4,11 +4,11 @@ import 'package:finance_manager/presentation/bloc/setting/setting_state.dart';
 import 'package:finance_manager/presentation/view/base/base_stateless_widget.dart';
 import 'package:finance_manager/presentation/view/widgets/appbar/custom_app_bar.dart';
 import 'package:finance_manager/presentation/view/widgets/dropdown/custom_drop_down.dart';
+import 'package:finance_manager/presentation/view/widgets/switch/custom_switch.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SettingScreen extends BaseStatelessWidget {
-
   const SettingScreen({super.key});
 
   @override
@@ -30,6 +30,14 @@ class SettingScreen extends BaseStatelessWidget {
                       if (value != null) {
                         getBloc<SettingCubit>(context).changeCurrency(value);
                       }
+                    },
+                  ),
+                  SizedBox(height: 16),
+                  CustomSwitch(
+                    label: 'Dark Theme',
+                    value: state.isDarkTheme,
+                    onChanged: (value) {
+                      getBloc<SettingCubit>(context).toggleDarkTheme(value);
                     },
                   ),
                 ],
