58d8da5ba4ce29f037889cf7195d583b1f41d6fa
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-17 17:48:14 +0300

    Add currency setting

--- lib/presentation/view/screens/setting/setting_screen.dart
+++ lib/presentation/view/screens/setting/setting_screen.dart
@@ -1,17 +1,43 @@
 import 'package:finance_manager/core/di/di.dart';
 import 'package:finance_manager/presentation/bloc/setting/setting_cubit.dart';
+import 'package:finance_manager/presentation/bloc/setting/setting_state.dart';
+import 'package:finance_manager/presentation/view/base/base_stateless_widget.dart';
 import 'package:finance_manager/presentation/view/widgets/appbar/custom_app_bar.dart';
+import 'package:finance_manager/presentation/view/widgets/dropdown/custom_drop_down.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class SettingScreen extends StatelessWidget {
+class SettingScreen extends BaseStatelessWidget {
+
   const SettingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (_) => getIt<SettingCubit>(),
-      child: Scaffold(appBar: CustomAppBar(title: 'Setting')),
+      child: Scaffold(
+        appBar: CustomAppBar(title: 'Settings'),
+        body: BlocBuilder<SettingCubit, SettingState>(
+          builder: (context, state) {
+            return Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  CustomDropdown(
+                    label: 'Currency',
+                    items: ['USD', 'EUR', 'IRR'],
+                    onChanged: (value) {
+                      if (value != null) {
+                        getBloc<SettingCubit>(context).changeCurrency(value);
+                      }
+                    },
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
