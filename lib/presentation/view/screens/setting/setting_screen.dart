66787015e7c14f8b6e13eccba73535c98aeeca84
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-17 17:07:24 +0300

    Add setting_screen.dart

---
+++ lib/presentation/view/screens/setting/setting_screen.dart
@@ -0,0 +1,16 @@
+import 'package:finance_manager/presentation/view/base/base_state.dart';
+import 'package:flutter/material.dart';
+
+class SettingScreen extends StatefulWidget {
+  const SettingScreen({super.key});
+
+  @override
+  State<SettingScreen> createState() => _SettingScreenState();
+}
+
+class _SettingScreenState extends BaseState<SettingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
