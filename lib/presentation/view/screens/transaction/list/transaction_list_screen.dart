980b244b81fedaa1629365d309e3fab7a698eb55
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-20 14:03:44 +0300

    Add transaction_list_screen.dart

---
+++ lib/presentation/view/screens/transaction/list/transaction_list_screen.dart
@@ -0,0 +1,11 @@
+import 'package:finance_manager/presentation/view/base/base_stateless_widget.dart';
+import 'package:flutter/material.dart';
+
+class TransactionListScreen extends BaseStatelessWidget {
+  const TransactionListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('transaction List'));
+  }
+}
