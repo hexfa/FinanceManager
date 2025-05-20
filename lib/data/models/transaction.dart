be7beeaf2fafe509c2edc5d3da7e634564bd4c0f
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-20 11:48:21 +0300

    Improve transaction model

--- lib/data/models/transaction.dart
+++ lib/data/models/transaction.dart
@@ -1,10 +1,12 @@
+import 'package:finance_manager/core/constants/currency_list.dart';
 import 'package:finance_manager/core/utils/category_type.dart';
 import 'package:finance_manager/core/utils/transaction_type.dart';
+import 'package:finance_manager/data/service/currency_service.dart';
 
 class Transaction {
   final int? id;
   final String title;
-  final double amount;
+  final double _amount;
   final TransactionType type;
   final CategoryType category;
   final DateTime date;
@@ -12,9 +14,15 @@ class Transaction {
   Transaction({
     this.id,
     required this.title,
-    required this.amount,
+    required double amount,
     required this.type,
     required this.category,
     required this.date,
-  });
+  }) : _amount = amount;
+
+  double get amount {
+    final currency = CurrencyService().currencyCode;
+    final rate = exchangeRates[currency] ?? 1.0;
+    return _amount * rate;
+  }
 }
