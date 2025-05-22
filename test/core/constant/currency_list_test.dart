c727c64dd8e4bd5a154ec6a7bea0b6914a7d0564
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-22 15:15:06 +0300

    Verify toCurrency returns correct Currency for valid codes

--- test/core/constant/currency_list_test.dart
+++ test/core/constant/currency_list_test.dart
@@ -2,5 +2,10 @@ import 'package:finance_manager/core/constants/currency_list.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-
+  group('CurrencyCodeExtension', () {
+    test('returns correct Currency for valid code', () {
+      expect('EUR'.toCurrency().name, 'Euro');
+      expect('JPY'.toCurrency().name, 'Japanese Yen');
+    });
+  });
 }
