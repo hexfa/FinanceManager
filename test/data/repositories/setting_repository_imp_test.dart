938d41c25b8b93381b837c8b8c9f433529d3ebb0
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-22 15:38:03 +0300

    Return USD as default if currency is not set

--- test/data/repositories/setting_repository_imp_test.dart
+++ test/data/repositories/setting_repository_imp_test.dart
@@ -1,8 +1,10 @@
 import 'package:finance_manager/data/repositories/setting_repository_imp.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:finance_manager/core/utils/configuration_key.dart';
 import 'package:finance_manager/data/datasource/sharepref_data_source.dart';
 import 'package:finance_manager/data/service/currency_service.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
 import 'setting_repository_imp_test.mocks.dart';
 
@@ -14,5 +16,13 @@ void main() {
   setUp(() {
     mockDataSource = MockShareprefDataSource();
     repository = SettingRepositoryImpl(mockDataSource);
+  });
+
+  test('getCurrency returns USD as default if not set', () {
+    when(mockDataSource.getString(ConfigurationKey.currency)).thenReturn(null);
+
+    final result = repository.getCurrency();
+
+    expect(result, 'USD');
   });
 }
