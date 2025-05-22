717cc99d8b6f986c92e2fe4d3e7c1db79148b8b8
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-22 15:23:38 +0300

    Add support for negative values in formatCurrencyFromString

--- test/core/utils/convert_string_test.dart
+++ test/core/utils/convert_string_test.dart
@@ -2,7 +2,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:finance_manager/core/utils/convert_string.dart';
 
 void main() {
-
   group('ConvertString', () {
     test('formatDate formats DateTime correctly', () {
       final date = DateTime(2024, 5, 15);
@@ -15,12 +14,18 @@ void main() {
       expect(ConvertString.formatCurrencyFromString('1000000'), '1,000,000');
       expect(ConvertString.formatCurrencyFromString(''), '');
       expect(ConvertString.formatCurrencyFromString('abc'), '0');
-      expect(ConvertString.formatCurrencyFromString('1,234'), '1,234'); // already formatted
+      expect(
+        ConvertString.formatCurrencyFromString('1,234'),
+        '1,234',
+      ); // already formatted
     });
 
     test('formatCurrencyFromDouble formats double values with commas', () {
       expect(ConvertString.formatCurrencyFromDouble(1000), '1,000');
-      expect(ConvertString.formatCurrencyFromDouble(1234567.89), '1,234,568'); // rounded
+      expect(
+        ConvertString.formatCurrencyFromDouble(1234567.89),
+        '1,234,568',
+      ); // rounded
       expect(ConvertString.formatCurrencyFromDouble(0), '0');
     });
 
@@ -29,6 +34,10 @@ void main() {
       expect(ConvertString.toDouble('123456.78'), 123456.78);
       expect(ConvertString.toDouble(''), 0.0);
       expect(ConvertString.toDouble('abc'), 0.0);
+    });
+
+    test('formatCurrencyFromString handles negative values', () {
+      expect(ConvertString.formatCurrencyFromString('-1000'), '-1,000');
     });
   });
 }
