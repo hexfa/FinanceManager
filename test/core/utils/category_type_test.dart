ad8fcbad7a00b0eebe42f23795323484846b2433
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-22 15:20:21 +0300

    Map valid lowercase strings to correct CategoryType values

--- test/core/utils/category_type_test.dart
+++ test/core/utils/category_type_test.dart
@@ -1,3 +1,13 @@
-void main() {
+import 'package:finance_manager/core/utils/category_type.dart';
+import 'package:flutter_test/flutter_test.dart';
 
+void main() {
+  group('CategoryTypeMapper', () {
+    test('returns correct CategoryType for valid lowercase strings', () {
+      expect('business'.toCategoryType(), CategoryType.business);
+      expect('food'.toCategoryType(), CategoryType.food);
+      expect('education'.toCategoryType(), CategoryType.education);
+      expect('sport'.toCategoryType(), CategoryType.sport);
+    });
+  });
 }
