8cf3871bf650e4b4008d28ab3f3bef7f737a93b6
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-24 18:18:34 +0300

    Add assets.gen.dart

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,27 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class $AssetsIconsGen {
+  const $AssetsIconsGen();
+
+  /// File path: assets/icons/expense.svg
+  String get expense => 'assets/icons/expense.svg';
+
+  /// File path: assets/icons/income.svg
+  String get income => 'assets/icons/income.svg';
+
+  /// List of all assets
+  List<String> get values => [expense, income];
+}
+
+class Assets {
+  const Assets._();
+
+  static const $AssetsIconsGen icons = $AssetsIconsGen();
+}
