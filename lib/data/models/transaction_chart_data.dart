b79421fd411c1ac4e650133f2b17fc1f951a5687
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-25 13:23:34 +0300

    Add transaction_chart_data.data

---
+++ lib/data/models/transaction_chart_data.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class TransactionChartData {
+  final String title;
+  final double percentage;
+  final Color color;
+
+  TransactionChartData({
+    required this.title,
+    required this.percentage,
+    required this.color,
+  });
+}
