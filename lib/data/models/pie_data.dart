d3af50853b2b2181b76c3d7c3d31c18ccdbeec61
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-25 13:23:08 +0300

    Remove pie_data.dart

--- lib/data/models/pie_data.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:ui';
-
-class PieData {
-  final String title;
-  final double percentage;
-  final Color color;
-
-  PieData({required this.title, required this.percentage, required this.color});
-}
