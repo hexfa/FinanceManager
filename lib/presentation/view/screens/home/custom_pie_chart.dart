effa60cdf94a3d360bf89c62d5b3fa2acfd61767
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-25 13:27:52 +0300

    Improve custom_pie_chart.dart

--- lib/presentation/view/screens/home/custom_pie_chart.dart
+++ lib/presentation/view/screens/home/custom_pie_chart.dart
@@ -1,17 +1,18 @@
-import 'package:finance_manager/data/models/pie_data.dart';
+import 'package:finance_manager/core/utils/convert_string.dart';
+import 'package:finance_manager/data/models/transaction_chart_data.dart';
 import 'package:finance_manager/presentation/view/base/base_stateless_widget.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 class CustomPieChart extends BaseStatelessWidget {
-  final List<PieData> data ;
+  final List<TransactionChartData> data;
 
   const CustomPieChart({super.key, required this.data});
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 1.3,
+    return SizedBox(
+      height: 200,
       child: PieChart(
         PieChartData(
           sections:
@@ -19,8 +20,9 @@ class CustomPieChart extends BaseStatelessWidget {
                   .map(
                     (data) => PieChartSectionData(
                       color: data.color,
-                      value: data.percentage,
-                      title: '${data.title}\n${data.percentage}%',
+                      value: ConvertString.roundToTwoDecimals(data.percentage),
+                      title:
+                          '${data.title}\n${ConvertString.formatCurrencyFromDouble(data.percentage)}%',
                       titleStyle: theme(context).textTheme.labelSmall?.copyWith(
                         color: theme(context).colorScheme.surface,
                       ),
