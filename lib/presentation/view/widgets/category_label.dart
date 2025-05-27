2aba198ae0c0f8ea901c741d7bae6dd6f11323db
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-27 21:57:16 +0300

    Add category_label.dart

---
+++ lib/presentation/view/widgets/category_label.dart
@@ -0,0 +1,29 @@
+import 'package:finance_manager/core/utils/category.dart';
+import 'package:finance_manager/core/utils/category_type.dart';
+import 'package:finance_manager/presentation/view/base/base_stateless_widget.dart';
+import 'package:flutter/material.dart';
+
+class CategoryLabel extends BaseStatelessWidget {
+  final CategoryType category;
+
+  const CategoryLabel({super.key, required this.category});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        getIcon(
+          Category.getCategoryIconPath(category),
+          color: theme(context).colorScheme.onSurface,
+        ),
+        const SizedBox(width: 8),
+        Text(
+          category.name,
+          style: theme(context).textTheme.titleMedium?.copyWith(
+            color: theme(context).colorScheme.onSurface,
+          ),
+        ),
+      ],
+    );
+  }
+}
