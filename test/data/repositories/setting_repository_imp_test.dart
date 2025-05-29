548e13ad56fd42726038161807898ddbea614be2
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-29 18:06:51 +0300

    Resolve bug related setting repo test

--- test/data/repositories/setting_repository_imp_test.dart
+++ test/data/repositories/setting_repository_imp_test.dart
@@ -1,36 +1,36 @@
-// import 'package:finance_manager/data/repositories/setting_repository_imp.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:finance_manager/core/utils/configuration_key.dart';
-// import 'package:finance_manager/data/datasource/sharepref_data_source.dart';
-// import 'package:finance_manager/data/service/currency_service.dart';
-// import 'package:mockito/annotations.dart';
-// import 'package:mockito/mockito.dart';
-//
-// import 'setting_repository_imp_test.mocks.dart';
-//
-// @GenerateMocks([ShareprefDataSource, CurrencyService])
-// void main() {
-//   late MockShareprefDataSource mockDataSource;
-//   late SettingRepositoryImpl repository;
-//
-//   setUp(() {
-//     mockDataSource = MockShareprefDataSource();
-//     repository = SettingRepositoryImpl(mockDataSource);
-//   });
-//
-//   test('getCurrency returns USD as default if not set', () {
-//     when(mockDataSource.getString(ConfigurationKey.currency)).thenReturn(null);
-//
-//     final result = repository.getCurrency();
-//
-//     expect(result, 'USD');
-//   });
-//
-//   test('getCurrency returns saved value if present', () {
-//     when(mockDataSource.getString(ConfigurationKey.currency)).thenReturn('EUR');
-//
-//     final result = repository.getCurrency();
-//
-//     expect(result, 'EUR');
-//   });
-// }
+import 'package:finance_manager/data/repositories/setting_repository_imp.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:finance_manager/core/utils/configuration_key.dart';
+import 'package:finance_manager/data/datasource/sharepref_data_source.dart';
+import 'package:finance_manager/data/service/currency_service.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'setting_repository_imp_test.mocks.dart';
+
+@GenerateMocks([ShareprefDataSource, CurrencyService])
+void main() {
+  late MockShareprefDataSource mockDataSource;
+  late SettingRepositoryImpl repository;
+
+  setUp(() {
+    mockDataSource = MockShareprefDataSource();
+    repository = SettingRepositoryImpl(mockDataSource);
+  });
+
+  test('getCurrency returns USD as default if not set', () {
+    when(mockDataSource.getString(ConfigurationKey.currency)).thenReturn(null);
+
+    final result = repository.getCurrency();
+
+    expect(result, 'USD');
+  });
+
+  test('getCurrency returns saved value if present', () {
+    when(mockDataSource.getString(ConfigurationKey.currency)).thenReturn('EUR');
+
+    final result = repository.getCurrency();
+
+    expect(result, 'EUR');
+  });
+}
