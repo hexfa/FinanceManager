9074cefc780f5a11a6bcf8ad9abe0fc028263498
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-02 11:46:22 +0300

    Add name category

--- lib/presentation/view/screens/category/create_category_screen.dart
+++ lib/presentation/view/screens/category/create_category_screen.dart
@@ -1,5 +1,6 @@
 import 'package:finance_manager/presentation/view/base/base_state.dart';
 import 'package:finance_manager/presentation/view/widgets/appbar/custom_app_bar.dart';
+import 'package:finance_manager/presentation/view/widgets/textfield/custom_text_field.dart';
 import 'package:flutter/material.dart';
 
 class CreateCategoryScreen extends StatefulWidget {
@@ -10,8 +11,31 @@ class CreateCategoryScreen extends StatefulWidget {
 }
 
 class _CreateCategoryScreenState extends BaseState<CreateCategoryScreen> {
+  late TextEditingController nameController;
+
+  @override
+  void initState() {
+    super.initState();
+    nameController = TextEditingController();
+
+    // nameController.addListener(() {
+    //   getBloc<TransactionCubit>().updateTitle(titleController.text);
+    // });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: CustomAppBar(title: 'Create Category'));
+    return Scaffold(
+      appBar: CustomAppBar(title: 'Create Category'),
+      body: Column(
+        children: [
+          CustomTextField(
+            prefixIcon: Icons.category_outlined,
+            hint: 'Name ...',
+            controller: nameController,
+          ),
+        ],
+      ),
+    );
   }
 }
