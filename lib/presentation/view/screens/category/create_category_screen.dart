b2d1c0c5166fc46d46b9d28d2fcdbac91aef4bda
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-02 11:22:43 +0300

    Add create_category_screen.dart

---
+++ lib/presentation/view/screens/category/create_category_screen.dart
@@ -0,0 +1,16 @@
+import 'package:finance_manager/presentation/view/base/base_state.dart';
+import 'package:flutter/material.dart';
+
+class CreateCategoryScreen extends StatefulWidget {
+  const CreateCategoryScreen({super.key});
+
+  @override
+  State<CreateCategoryScreen> createState() => _CreateCategoryScreenState();
+}
+
+class _CreateCategoryScreenState extends BaseState<CreateCategoryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(child: Text(''));
+  }
+}
