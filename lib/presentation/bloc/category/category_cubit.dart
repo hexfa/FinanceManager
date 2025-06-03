a2fe53749f16f601ba17f01bfcd286c9ff4cd6d0
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-03 22:20:24 +0300

    Add category cubit

---
+++ lib/presentation/bloc/category/category_cubit.dart
@@ -0,0 +1,14 @@
+import 'package:finance_manager/domain/repositories/category_repository.dart';
+import 'package:finance_manager/presentation/bloc/base/base_cubit.dart';
+import 'package:finance_manager/presentation/bloc/category/category_state.dart';
+import 'package:finance_manager/data/models/category.dart';
+
+class CategoryCubit extends BaseCubit<CategoryState> {
+  final CategoryRepository repository;
+
+  CategoryCubit(this.repository) : super(CategoryState());
+
+  Future<void> create(String name) async {
+    await repository.createCategory(Category(id: 0, name: name));
+  }
+}
