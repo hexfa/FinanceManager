b406c5b09759c9696b941483200ffa379204a94c
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-03 22:22:50 +0300

    Clean code

--- lib/presentation/bloc/category/category_cubit.dart
+++ lib/presentation/bloc/category/category_cubit.dart
@@ -6,7 +6,7 @@ import 'package:finance_manager/data/models/category.dart';
 class CategoryCubit extends BaseCubit<CategoryState> {
   final CategoryRepository repository;
 
-  CategoryCubit(this.repository) : super(CategoryState());
+  CategoryCubit({required this.repository}) : super(CategoryState());
 
   Future<void> create(String name) async {
     await repository.createCategory(Category(id: 0, name: name));
