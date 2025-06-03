441e3e70bf02321aa53588a20bb69a173413bdf6
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-03 22:19:10 +0300

    Add category state

---
+++ lib/presentation/bloc/category/category_state.dart
@@ -0,0 +1,7 @@
+import 'package:finance_manager/data/models/transaction_chart_data.dart';
+import 'package:finance_manager/data/models/transaction.dart';
+import 'package:finance_manager/presentation/bloc/base/base_bloc_state.dart';
+
+class CategoryState extends BaseBlocState {
+  const CategoryState();
+}
