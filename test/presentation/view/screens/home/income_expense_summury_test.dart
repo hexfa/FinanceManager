54d049f1f02aff58ac4835ac65bdc8c812c85c8e
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-04 13:43:58 +0300

    Add widget test for displaying income and expense values

--- test/presentation/view/screens/home/income_expense_summury_test.dart
+++ test/presentation/view/screens/home/income_expense_summury_test.dart
@@ -1,1 +1,18 @@
-void main() {}
+import 'package:finance_manager/presentation/view/screens/home/income_expense_summury.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('displays income and expense values', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: IncomeExpenseSummary(income: '2000 \$', expense: '1500 \$'),
+      ),
+    );
+
+    expect(find.text('2000 \$'), findsOneWidget);
+    expect(find.text('1500 \$'), findsOneWidget);
+  });
+}
