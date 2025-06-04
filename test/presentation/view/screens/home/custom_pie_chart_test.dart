da3a66a0a1a23600212c92bc378cfbeb714e684b
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-04 13:21:40 +0300

    Ensure PieChart widget is rendered inside CustomPieChart

--- test/presentation/view/screens/home/custom_pie_chart_test.dart
+++ test/presentation/view/screens/home/custom_pie_chart_test.dart
@@ -1,1 +1,27 @@
-void main() {}
+import 'package:finance_manager/data/models/transaction_chart_data.dart';
+import 'package:finance_manager/presentation/view/screens/home/custom_pie_chart.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('renders PieChart widget', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: CustomPieChart(
+            data: [
+              TransactionChartData(
+                title: 'Food',
+                percentage: 40,
+                color: Colors.red,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+
+    expect(find.byType(PieChart), findsOneWidget);
+  });
+}
