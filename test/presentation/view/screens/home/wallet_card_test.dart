5e926904002c05aa6ea02cbfe42d98a9e1f78b10
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-05 08:56:06 +0300

    Ensure balance is displayed correctly in WalletCard

--- test/presentation/view/screens/home/wallet_card_test.dart
+++ test/presentation/view/screens/home/wallet_card_test.dart
@@ -1,1 +1,22 @@
-void main() {}
+import 'package:finance_manager/presentation/view/screens/home/wallet_card.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('displays correct balance in CurrencyLabel', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: WalletCard(
+          balance: 1234.56,
+          income: 0,
+          expense: 0,
+          lastUpdate: '2h ago',
+        ),
+      ),
+    );
+
+    expect(find.textContaining('1234.56'), findsOneWidget);
+  });
+}
