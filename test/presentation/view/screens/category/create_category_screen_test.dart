a2a443f8473b09d484e70035886562a669d82ae3
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-05 09:09:39 +0300

    Ensure essential widgets are present in CreateCategoryScreen

--- test/presentation/view/screens/category/create_category_screen_test.dart
+++ test/presentation/view/screens/category/create_category_screen_test.dart
@@ -1,1 +1,16 @@
-void main() {}
+import 'package:finance_manager/presentation/view/screens/category/create_category_screen.dart';
+import 'package:finance_manager/presentation/view/widgets/button/custom_button.dart';
+import 'package:finance_manager/presentation/view/widgets/textfield/custom_text_field.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('renders CustomTextField and CustomButton', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(MaterialApp(home: const CreateCategoryScreen()));
+
+    expect(find.byType(CustomTextField), findsOneWidget);
+    expect(find.byType(CustomButton), findsOneWidget);
+  });
+}
