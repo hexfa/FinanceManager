f6f2d003a4f5f573d5d8b35c1ee1fd569aed61e3
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-05 09:17:07 +0300

    Prevent create call when category name is empty

--- test/presentation/view/screens/category/create_category_screen_test.dart
+++ test/presentation/view/screens/category/create_category_screen_test.dart
@@ -2,9 +2,17 @@ import 'package:finance_manager/presentation/view/screens/category/create_catego
 import 'package:finance_manager/presentation/view/widgets/button/custom_button.dart';
 import 'package:finance_manager/presentation/view/widgets/textfield/custom_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import 'mock_category_cubit_test.dart';
 
 void main() {
+  setUpAll(() {
+    registerFallbackValue('');
+  });
+
   testWidgets('renders CustomTextField and CustomButton', (
     WidgetTester tester,
   ) async {
@@ -12,5 +20,25 @@ void main() {
 
     expect(find.byType(CustomTextField), findsOneWidget);
     expect(find.byType(CustomButton), findsOneWidget);
+  });
+
+  testWidgets('does not call create when text is empty', (
+    WidgetTester tester,
+  ) async {
+    final mockCubit = MockCategoryCubit();
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: BlocProvider.value(
+          value: mockCubit,
+          child: const CreateCategoryScreen(),
+        ),
+      ),
+    );
+
+    await tester.tap(find.byType(CustomButton));
+    await tester.pump();
+
+    // verifyNever(() => mockCubit.create(any()));
   });
 }
