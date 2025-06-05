1c22932e49e2f33dcfc757b29df7ba9f4182928f
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-05 09:16:46 +0300

    Improve mock_category_cubit_test.dart

--- test/presentation/view/screens/category/mock_category_cubit_test.dart
+++ test/presentation/view/screens/category/mock_category_cubit_test.dart
@@ -1,1 +1,4 @@
-void main() {}
+import 'package:mocktail/mocktail.dart';
+import 'package:finance_manager/presentation/bloc/category/category_cubit.dart';
+
+class MockCategoryCubit extends Mock implements CategoryCubit {}
