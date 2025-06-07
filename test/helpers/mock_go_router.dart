456bf3d808af5889775358f43bd5a4b087621a60
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-07 19:06:46 +0330

    Implement mock_go_router.dart

--- test/helpers/mock_go_router.dart
+++ test/helpers/mock_go_router.dart
@@ -1,4 +1,15 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mockito/mockito.dart';
 
-class MockGoRouter extends Mock implements GoRouter {}
+class MockGoRouter extends Mock implements GoRouter {
+  @override
+  RouterDelegate get routerDelegate => MockRouterDelegate();
+
+  @override
+  RouteInformationParser get routeInformationParser => MockRouteInformationParser();
+
+  @override
+  void push(String location) {}
+}
+}
