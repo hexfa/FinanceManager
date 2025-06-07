8b3b0370db32cad0f82dc1c1ab539ad120967ba7
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-07 19:46:49 +0330

    Verify that CustomAppBar displays the correct title

--- test/presentation/view/widgets/custom_app_bar_test.dart
+++ test/presentation/view/widgets/custom_app_bar_test.dart
@@ -1,1 +1,15 @@
-void main() {}
+import 'package:finance_manager/presentation/view/widgets/appbar/custom_app_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('verify title is rendered correctly in CustomAppBar', (tester) async {
+    await tester.pumpWidget(MaterialApp(
+      home: Scaffold(
+        appBar: CustomAppBar(title: 'Test Title'),
+      ),
+    ));
+
+    expect(find.text('Test Title'), findsOneWidget);
+  });
+}
