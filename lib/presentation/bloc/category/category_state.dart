a6d6a3e6f0712c777b6aa7d41eb60e96274b345d
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-08 16:19:07 +0330

    Update category_state.dart

--- lib/presentation/bloc/category/category_state.dart
+++ lib/presentation/bloc/category/category_state.dart
@@ -1,5 +1,12 @@
+import 'package:finance_manager/data/models/category.dart';
 import 'package:finance_manager/presentation/bloc/base/base_bloc_state.dart';
 
 class CategoryState extends BaseBlocState {
-  const CategoryState();
+  final List<Category> categories;
+
+  const CategoryState({this.categories = const []});
+
+  CategoryState copyWith({List<Category>? categories}) {
+    return CategoryState(categories: categories ?? this.categories);
+  }
 }
