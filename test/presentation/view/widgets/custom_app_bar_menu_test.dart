50c60b08b0d77ffae177d5c3b4bdd4db81a74673
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-09 22:30:20 +0330

    Verify more_vert icon is displayed in CustomAppBarMenu

--- test/presentation/view/widgets/custom_app_bar_menu_test.dart
+++ test/presentation/view/widgets/custom_app_bar_menu_test.dart
@@ -1,1 +1,17 @@
-void main() {}
+import 'package:finance_manager/presentation/view/widgets/appbar/custom_app_bar_menu.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../../helpers/build_testable_widget.dart';
+
+void main() {
+  testWidgets('verify more_vert icon is displayed in CustomAppBarMenu', (
+    tester,
+  ) async {
+    await tester.pumpWidget(
+      buildTestableWidget(CustomAppBarMenu(menuItem: [])),
+    );
+
+    expect(find.byIcon(Icons.more_vert), findsOneWidget);
+  });
+}
