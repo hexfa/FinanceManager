5509d764bcd67e9b29e13ce30be4e17650d9fe6a
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-09 22:20:14 +0330

    Verify all widgets render correctly in SettingScreen

--- test/presentation/view/screens/setting/setting_screen_test.dart
+++ test/presentation/view/screens/setting/setting_screen_test.dart
@@ -1,1 +1,19 @@
-void main() {}
+import 'package:finance_manager/data/models/currency.dart';
+import 'package:finance_manager/presentation/view/screens/setting/setting_screen.dart';
+import 'package:finance_manager/presentation/view/widgets/dropdown/custom_drop_down.dart';
+import 'package:finance_manager/presentation/view/widgets/switch/custom_switch.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../../../helpers/build_testable_widget.dart';
+
+void main() {
+  testWidgets('should display all widgets in SettingScreen', (tester) async {
+    await tester.pumpWidget(buildTestableWidget(const SettingScreen()));
+
+    expect(find.text('Country'), findsOneWidget);
+    expect(find.byType(CustomDropdown<Currency>), findsOneWidget);
+    expect(find.text('Dark Theme'), findsOneWidget);
+    expect(find.byType(CustomSwitch), findsOneWidget);
+    expect(find.text('Create Category'), findsOneWidget);
+  });
+}
