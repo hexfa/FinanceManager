80e95af57633c0696fc7288e9e89f2fb3eaaf52a
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-09 22:21:43 +0330

    Verify currency change triggers cubit method

--- test/presentation/view/screens/setting/setting_screen_test.dart
+++ test/presentation/view/screens/setting/setting_screen_test.dart
@@ -1,3 +1,4 @@
+import 'package:finance_manager/core/constants/currency.dart';
 import 'package:finance_manager/data/models/currency.dart';
 import 'package:finance_manager/presentation/view/screens/setting/setting_screen.dart';
 import 'package:finance_manager/presentation/view/widgets/dropdown/custom_drop_down.dart';
@@ -5,6 +6,7 @@ import 'package:finance_manager/presentation/view/widgets/switch/custom_switch.d
 import 'package:flutter_test/flutter_test.dart';
 
 import '../../../../helpers/build_testable_widget.dart';
+import '../../../../helpers/setting_cubit_fake.dart';
 
 void main() {
   testWidgets('should display all widgets in SettingScreen', (tester) async {
@@ -15,5 +17,24 @@ void main() {
     expect(find.text('Dark Theme'), findsOneWidget);
     expect(find.byType(CustomSwitch), findsOneWidget);
     expect(find.text('Create Category'), findsOneWidget);
+  });
+
+  testWidgets('should call changeCurrency when a new currency is selected', (
+    tester,
+  ) async {
+    final cubit = SettingCubitFake();
+    await tester.pumpWidget(
+      buildTestableWidget(const SettingScreen(), settingCubit: cubit),
+    );
+
+    await tester.tap(find.byType(CustomDropdown<Currency>));
+    await tester.pumpAndSettle();
+
+    await tester.tap(
+      find.text('${currencyList[1].code} - ${currencyList[1].name}').last,
+    );
+    await tester.pump();
+
+    expect(cubit.changedCurrencyCode, currencyList[1]);
   });
 }
