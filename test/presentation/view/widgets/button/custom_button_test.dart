ca6d1531cc43bf243532c70f5fb3d74afe0a141c
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-11 19:48:56 +0330

    Verify that CustomButton displays correct text

--- test/presentation/view/widgets/button/custom_button_test.dart
+++ test/presentation/view/widgets/button/custom_button_test.dart
@@ -1,1 +1,21 @@
-void main() {}
+import 'package:finance_manager/presentation/view/widgets/button/custom_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+
+  testWidgets('should display the correct text', (tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: CustomButton(
+            text: 'Save',
+            onPressed: () {},
+          ),
+        ),
+      ),
+    );
+
+    expect(find.text('Save'), findsOneWidget);
+  });
+}
