ff2a46b851fa9282095ad9b770bc10e471908e9d
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-12 10:46:52 +0330

    Clean code

--- test/presentation/view/screens/transaction/create/create_transaction_screen_test.dart
+++ test/presentation/view/screens/transaction/create/create_transaction_screen_test.dart
@@ -7,7 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
-import '../../../../helpers/mock_cubit.dart';
+import '../../../../../helpers/mock_cubit.dart';
 
 void main() {
   testWidgets('renders app bar with title and create button', (tester) async {
