6bc1aa9b8c3c620d4907f5ad8a3bf564561f5738
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-12 10:34:24 +0330

    Ensure CreateTransactionScreen shows correct app bar title and action button text

--- test/presentation/view/screens/transaction/create_transaction_screen_test.dart
+++ test/presentation/view/screens/transaction/create_transaction_screen_test.dart
@@ -1,1 +1,22 @@
-void main() {}
+import 'package:finance_manager/presentation/bloc/transaction/transaction_cubit.dart';
+import 'package:finance_manager/presentation/view/screens/transaction/create/create_transaction_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import '../../../../helpers/mock_cubit.dart';
+
+void main() {
+  testWidgets('renders app bar with title and create button', (tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: BlocProvider<TransactionCubit>(
+          create: (_) => MockTransactionCubit(),
+          child: const CreateTransactionScreen(),
+        ),
+      ),
+    );
+
+    expect(find.text('Add Transaction'), findsOneWidget);
+    expect(find.text('Create'), findsOneWidget);
+  });
+}
