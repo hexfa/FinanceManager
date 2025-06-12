5ff252aa98bfcb28ac5292c55334afef6af30b66
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-12 10:50:14 +0330

    Verify UpdateTransactionScreen initializes fields with transaction data

--- test/presentation/view/screens/transaction/update/update_transaction_screen_test.dart
+++ test/presentation/view/screens/transaction/update/update_transaction_screen_test.dart
@@ -1,1 +1,45 @@
-void main() {}
+import 'package:finance_manager/core/utils/transaction_type.dart';
+import 'package:finance_manager/data/models/category.dart';
+import 'package:finance_manager/data/models/transaction.dart';
+import 'package:finance_manager/presentation/bloc/transaction/transaction_cubit.dart';
+import 'package:finance_manager/presentation/bloc/transaction/transaction_state.dart';
+import 'package:finance_manager/presentation/view/screens/transaction/update/update_transaction_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../../../../helpers/mock_cubit.dart';
+
+void main() {
+  testWidgets('initializes fields with transaction data', (tester) async {
+    final transaction = Transaction(
+      id: 1,
+      title: 'Rent',
+      amount: 5000,
+      category: Category(id: 2, name: 'Housing'),
+      type: TransactionType.expense,
+      date: DateTime(2024, 1, 1),
+      description: '',
+    );
+
+    final mockCubit = MockTransactionCubit();
+    when(
+      () => mockCubit.state,
+    ).thenReturn(TransactionState() as TransactionState Function());
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: BlocProvider<TransactionCubit>.value(
+          value: mockCubit,
+          child: UpdateTransactionScreen(transaction: transaction),
+        ),
+      ),
+    );
+
+    await tester.pump();
+
+    expect(find.widgetWithText(TextField, 'Rent'), findsOneWidget);
+    expect(find.text('5000'), findsOneWidget);
+  });
+}
