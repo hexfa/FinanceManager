c152c707a36fb6e2609f576dbbe17bb066ad42e1
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-15 18:45:15 +0330

    Update extension.dart

--- lib/core/extension/extension.dart
+++ lib/core/extension/extension.dart
@@ -12,7 +12,7 @@ extension TransactionEntityMapper on TransactionEntity {
       description: description,
       amount: amount,
       type: type,
-      category: category.toModel(),
+      category: category,
       date: date,
     );
   }
@@ -25,7 +25,7 @@ extension TransactionModelMapper on Transaction {
       description: description,
       amount: amount,
       type: type,
-      category: category.toEntity(),
+      category: category,
       date: date,
     );
   }
