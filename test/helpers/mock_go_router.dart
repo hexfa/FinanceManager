b24e468fa8d84a91a13ca931d79e3d6194cb6799
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-15 19:02:15 +0330

    Clean code in mock_go_router.dart

--- test/helpers/mock_go_router.dart
+++ test/helpers/mock_go_router.dart
@@ -3,13 +3,12 @@ import 'package:go_router/go_router.dart';
 import 'package:mockito/mockito.dart';
 
 class MockGoRouter extends Mock implements GoRouter {
-  @override
-  RouterDelegate get routerDelegate => MockRouterDelegate();
-
-  @override
-  RouteInformationParser get routeInformationParser => MockRouteInformationParser();
-
-  @override
-  void push(String location) {}
-}
+  // @override
+  // RouterDelegate get routerDelegate => MockRouterDelegate();
+  //
+  // @override
+  // RouteInformationParser get routeInformationParser => MockRouteInformationParser();
+  //
+  // @override
+  // void push(String location) {}
 }
