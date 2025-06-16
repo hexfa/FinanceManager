20eb36e93a104f6ebdc7a2575b98280bc755f8b4
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-16 16:55:34 +0330

    Clean code

--- test/data/repositories/transaction_repository_imp_test.dart
+++ test/data/repositories/transaction_repository_imp_test.dart
@@ -1,6 +1,6 @@
 import 'package:finance_manager/core/extension/extension.dart';
-import 'package:finance_manager/core/utils/category_type.dart';
 import 'package:finance_manager/core/utils/transaction_type.dart';
+import 'package:finance_manager/data/models/category.dart';
 import 'package:finance_manager/data/repositories/transaction_repository_imp.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
@@ -19,7 +19,7 @@ void main() {
     description: "Description",
     amount: 100.0,
     type: TransactionType.income,
-    category: CategoryType.sport,
+    category: Category(id: 1, name: 'sport'),
     date: DateTime(2023, 1, 1),
   );
 
