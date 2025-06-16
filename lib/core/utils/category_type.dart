e2774e3157ad207a9532bdebedaefaa9db1e30c7
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-16 22:24:38 +0330

    Remove category_type.dart

--- lib/core/utils/category_type.dart
+++ lib/core/utils/category_type.dart
@@ -1,10 +1,10 @@
-enum CategoryType { business, food, education, sport, other }
-
-extension CategoryTypeMapper on String {
-  CategoryType toCategoryType() {
-    return CategoryType.values.firstWhere(
-      (e) => e.name == toLowerCase(),
-      orElse: () => CategoryType.other,
-    );
-  }
-}
+// enum CategoryType { business, food, education, sport, other }
+//
+// extension CategoryTypeMapper on String {
+//   CategoryType toCategoryType() {
+//     return CategoryType.values.firstWhere(
+//       (e) => e.name == toLowerCase(),
+//       orElse: () => CategoryType.other,
+//     );
+//   }
+// }
