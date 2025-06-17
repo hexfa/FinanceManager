0abe2d90271f8fdc21e0a7a5ad04750bca8f8071
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-17 18:50:17 +0330

    Clean code

--- lib/presentation/view/screens/transaction/update/update_transaction_screen.dart
+++ lib/presentation/view/screens/transaction/update/update_transaction_screen.dart
@@ -1,4 +1,3 @@
-import 'package:finance_manager/data/models/category.dart';
 import 'package:finance_manager/data/models/transaction.dart';
 import 'package:finance_manager/presentation/bloc/transaction/transaction_cubit.dart';
 import 'package:finance_manager/presentation/view/screens/transaction/base/base_create_update_transaction.dart';
