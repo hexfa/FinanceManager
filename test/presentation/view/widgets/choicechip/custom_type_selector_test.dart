6062664843963a31e422ce18d14be4d537e80284
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-18 10:34:07 +0330

    Display income and expense chips in CustomTypeSelector

--- test/presentation/view/widgets/choicechip/custom_type_selector_test.dart
+++ test/presentation/view/widgets/choicechip/custom_type_selector_test.dart
@@ -1,1 +1,20 @@
-void main() {}
+import 'package:finance_manager/core/utils/transaction_type.dart';
+import 'package:finance_manager/presentation/view/widgets/choicechip/custom_type_selector.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('displays both income and expense chips', (tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: CustomTypeSelector(
+          selectedType: TransactionType.income,
+          onChanged: (_) {},
+        ),
+      ),
+    );
+
+    expect(find.text('income'), findsOneWidget);
+    expect(find.text('expense'), findsOneWidget);
+  });
+}
