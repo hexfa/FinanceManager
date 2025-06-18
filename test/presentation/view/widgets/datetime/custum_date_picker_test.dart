7f9eefa28307c4c9cfeb019fd63eeedce4aafada
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-18 10:44:54 +0330

    Display current date and time if no initialDateTime is given

--- test/presentation/view/widgets/datetime/custum_date_picker_test.dart
+++ test/presentation/view/widgets/datetime/custum_date_picker_test.dart
@@ -1,1 +1,16 @@
-void main() {}
+import 'package:finance_manager/presentation/view/widgets/datetime/custom_date_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('displays current datetime if no initial value is provided', (
+    tester,
+  ) async {
+    await tester.pumpWidget(
+      MaterialApp(home: DateTimePickerContainer(onDateTimeSelected: (_) {})),
+    );
+
+    final now = DateTime.now();
+    expect(find.textContaining('${now.year}'), findsOneWidget);
+  });
+}
