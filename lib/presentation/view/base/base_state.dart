6a567e8d449ac89941673902643d4ef6cb531071
@hexfa <amir.dehdarian2009@gmail.com>
2025-10-20 15:30:53 +0330

    Update base_state.dart.dart

--- lib/presentation/view/base/base_state.dart
+++ lib/presentation/view/base/base_state.dart
@@ -1,8 +1,8 @@
 import 'dart:io';
 
+import 'package:finance_manager/l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:go_router/go_router.dart';
 
 /// BaseState is a reusable class that provides common functionality
