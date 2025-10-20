113f8247e6bca78db2e8f06f519ec44b3efc3abc
@hexfa <amir.dehdarian2009@gmail.com>
2025-10-20 15:30:28 +0330

    Add app_localizations_fa.dart

---
+++ lib/l10n/app_localizations_fa.dart
@@ -0,0 +1,100 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Persian (`fa`).
+class AppLocalizationsFa extends AppLocalizations {
+  AppLocalizationsFa([String locale = 'fa']) : super(locale);
+
+  @override
+  String get balance => 'موجودی';
+
+  @override
+  String get setting => 'تنظیم';
+
+  @override
+  String get income => 'درآمد';
+
+  @override
+  String get expense => 'هزینه';
+
+  @override
+  String get transactions => 'تراکنش‌ها';
+
+  @override
+  String get seeAll => 'مشاهده همه';
+
+  @override
+  String get addTransaction => 'افزودن تراکنش';
+
+  @override
+  String get title => 'عنوان';
+
+  @override
+  String get amount => 'مبلغ';
+
+  @override
+  String get category => 'دسته‌بندی';
+
+  @override
+  String get ok => 'تأیید';
+
+  @override
+  String get detailTransaction => 'جزئیات تراکنش';
+
+  @override
+  String get update => 'به‌روزرسانی';
+
+  @override
+  String get delete => 'حذف';
+
+  @override
+  String get deleteTransaction => 'حذف تراکنش';
+
+  @override
+  String get areYouSureYouWantToDeleteThisTransaction => 'آیا از حذف این تراکنش مطمئن هستید؟';
+
+  @override
+  String get cancel => 'انصراف';
+
+  @override
+  String get settings => 'تنظیمات';
+
+  @override
+  String get currency => 'واحد پول';
+
+  @override
+  String get darkTheme => 'تم تیره';
+
+  @override
+  String get country => 'کشور';
+
+  @override
+  String get createCategory => 'ایجاد دسته بندی';
+
+  @override
+  String get name => 'نام';
+
+  @override
+  String get create => 'ایجاد';
+
+  @override
+  String get noTransactionsWereRecorded => 'No transactions were recorded.';
+
+  @override
+  String get expenseChart => 'Expense Chart';
+
+  @override
+  String get financeManager => 'Finance Manager';
+
+  @override
+  String get updated => 'Updated';
+
+  @override
+  String get other => 'Other';
+
+  @override
+  String get description => 'Description';
+}
