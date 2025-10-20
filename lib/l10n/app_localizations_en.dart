b2d6298ca311e7accf7f03b8ff186adbc614ffc0
@hexfa <amir.dehdarian2009@gmail.com>
2025-10-20 15:30:19 +0330

    Add app_localizations_en.dart

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,100 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get balance => 'Balance';
+
+  @override
+  String get setting => 'Setting';
+
+  @override
+  String get income => 'Income';
+
+  @override
+  String get expense => 'Expense';
+
+  @override
+  String get transactions => 'Transactions';
+
+  @override
+  String get seeAll => 'See all';
+
+  @override
+  String get addTransaction => 'Add Transaction';
+
+  @override
+  String get title => 'Title';
+
+  @override
+  String get amount => 'Amount';
+
+  @override
+  String get category => 'Category';
+
+  @override
+  String get ok => 'OK';
+
+  @override
+  String get detailTransaction => 'Detail Transaction';
+
+  @override
+  String get update => 'Update';
+
+  @override
+  String get delete => 'Delete';
+
+  @override
+  String get deleteTransaction => 'Delete Transaction';
+
+  @override
+  String get areYouSureYouWantToDeleteThisTransaction => 'Are you sure you want to delete this transaction?';
+
+  @override
+  String get cancel => 'Cancel';
+
+  @override
+  String get settings => 'Settings';
+
+  @override
+  String get currency => 'Currency';
+
+  @override
+  String get darkTheme => 'Dark Theme';
+
+  @override
+  String get country => 'Country';
+
+  @override
+  String get createCategory => 'Create Category';
+
+  @override
+  String get name => 'Name';
+
+  @override
+  String get create => 'Create';
+
+  @override
+  String get noTransactionsWereRecorded => 'No transactions were recorded.';
+
+  @override
+  String get expenseChart => 'Expense Chart';
+
+  @override
+  String get financeManager => 'Finance Manager';
+
+  @override
+  String get updated => 'Updated';
+
+  @override
+  String get other => 'Other';
+
+  @override
+  String get description => 'Description';
+}
