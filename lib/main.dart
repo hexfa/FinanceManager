6cb60f5eef06355e73f03fb76099566ed4ce139c
@hexfa <amir.dehdarian2009@gmail.com>
2025-11-18 21:06:34 +0300

    Clean code

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,6 @@ void main() async {
   Hive.registerAdapter(TransactionTypeAdapter());
   Hive.registerAdapter(TransactionAdapter());
   Hive.registerAdapter(CategoryAdapter());
-
   Hive.registerAdapter(CategoryEntityAdapter());
 
   await Hive.openBox<TransactionEntity>('transactions');
