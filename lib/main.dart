d629c1b0e835e8581514be6fcfd8b1d509e1abba
@hexfa <amir.dehdarian2009@gmail.com>
2025-11-18 21:06:14 +0300

    Clean code

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,6 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   Hive.registerAdapter(TransactionTypeAdapter());
-
   Hive.registerAdapter(TransactionAdapter());
 
   Hive.registerAdapter(CategoryAdapter());
