0b28e9bcfc788a36dd192aa0b38471c2c9274503
@hexfa <amir.dehdarian2009@gmail.com>
2025-11-18 21:07:18 +0300

    Clean code

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,6 @@ void main() async {
   await Hive.openBox<TransactionEntity>('transactions');
   await setup();
   final prefs = await SharedPreferences.getInstance();
-
   final isDarkTheme = prefs.getBool(ConfigurationKey.darkTheme) ?? false;
 
   final currency = prefs.getString(ConfigurationKey.currency) ?? 'USD';
