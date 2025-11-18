20456c6ca3c6de7f306536ffb313457c8f54829d
@hexfa <amir.dehdarian2009@gmail.com>
2025-11-18 21:06:55 +0300

    Clean code

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,6 @@ void main() async {
   Hive.registerAdapter(CategoryAdapter());
   Hive.registerAdapter(CategoryEntityAdapter());
   await Hive.openBox<TransactionEntity>('transactions');
-
   await setup();
 
   final prefs = await SharedPreferences.getInstance();
