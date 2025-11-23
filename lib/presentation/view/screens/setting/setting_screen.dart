dedb1f97de61e14e8964696e4fd54b953e9a40ae
@hexfa <amir.dehdarian2009@gmail.com>
2025-11-23 17:58:24 +0300

    Clean

--- lib/presentation/view/screens/setting/setting_screen.dart
+++ lib/presentation/view/screens/setting/setting_screen.dart
@@ -32,7 +32,6 @@ class SettingScreen extends BaseStatelessWidget {
                   ),
                 ),
                 SizedBox(height: 4),
-
                 CustomDropdown<Currency>(
 
                   items: currencyList,
