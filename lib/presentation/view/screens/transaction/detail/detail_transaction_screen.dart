5c616e8baabcf751b39154386fb4158e122a0a87
@hexfa <amir.dehdarian2009@gmail.com>
2025-11-26 17:24:14 +0300

    Clean code

--- lib/presentation/view/screens/transaction/detail/detail_transaction_screen.dart
+++ lib/presentation/view/screens/transaction/detail/detail_transaction_screen.dart
@@ -17,7 +17,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class DetailTransactionScreen extends StatefulWidget {
-
   final int transactionId;
 
   const DetailTransactionScreen({super.key, required this.transactionId});
