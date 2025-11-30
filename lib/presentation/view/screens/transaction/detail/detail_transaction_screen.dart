ed9626c00638f6ff7856a7ff91e0a3e8c13dad9b
@hexfa <amir.dehdarian2009@gmail.com>
2025-11-30 20:46:23 +0300

    Clean code

--- lib/presentation/view/screens/transaction/detail/detail_transaction_screen.dart
+++ lib/presentation/view/screens/transaction/detail/detail_transaction_screen.dart
@@ -74,7 +74,6 @@ class _DetailTransactionScreenState extends BaseState<DetailTransactionScreen>
                           ConvertString.formatDate(
                             transaction?.date ?? DateTime.now(),
                           ),
-
                           style: theme.textTheme.labelMedium?.copyWith(
 
                             color: theme.colorScheme.tertiary,
