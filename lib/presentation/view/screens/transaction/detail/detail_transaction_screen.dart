e591694970202252d10d7e753e16b0766ba49577
@hexfa <amir.dehdarian2009@gmail.com>
2025-11-30 20:47:48 +0300

    Clean code

--- lib/presentation/view/screens/transaction/detail/detail_transaction_screen.dart
+++ lib/presentation/view/screens/transaction/detail/detail_transaction_screen.dart
@@ -82,7 +82,6 @@ class _DetailTransactionScreenState extends BaseState<DetailTransactionScreen>
                     ),
                     //amount
                     _buildAmount(),
-
                   ],
 
                 ),
