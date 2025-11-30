c08d722049165b922b8f9d2af9a3a7e3eb58454c
@hexfa <amir.dehdarian2009@gmail.com>
2025-11-30 20:47:19 +0300

    Clean code

--- lib/presentation/view/screens/transaction/detail/detail_transaction_screen.dart
+++ lib/presentation/view/screens/transaction/detail/detail_transaction_screen.dart
@@ -77,7 +77,6 @@ class _DetailTransactionScreenState extends BaseState<DetailTransactionScreen>
                           style: theme.textTheme.labelMedium?.copyWith(
                             color: theme.colorScheme.tertiary,
                           ),
-
                         ),
 
                       ],
