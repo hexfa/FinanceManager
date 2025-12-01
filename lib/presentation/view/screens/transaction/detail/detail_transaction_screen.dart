a348fcc3fd0b8e5d8984b05c08c41bf5c32b665f
@hexfa <amir.dehdarian2009@gmail.com>
2025-12-01 15:01:52 +0300

    Clean code

--- lib/presentation/view/screens/transaction/detail/detail_transaction_screen.dart
+++ lib/presentation/view/screens/transaction/detail/detail_transaction_screen.dart
@@ -104,7 +104,6 @@ class _DetailTransactionScreenState extends BaseState<DetailTransactionScreen>
   Container buildDescription() {
     return Container(
       width: double.infinity,
-
       height: 200,
 
       padding: const EdgeInsets.all(12),
