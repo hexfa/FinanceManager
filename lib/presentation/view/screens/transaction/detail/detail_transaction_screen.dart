d7a9fb4370f5b9e057296366f95666b4aee9d1a9
@hexfa <amir.dehdarian2009@gmail.com>
2025-12-01 14:00:28 +0300

    Clean code

--- lib/presentation/view/screens/transaction/detail/detail_transaction_screen.dart
+++ lib/presentation/view/screens/transaction/detail/detail_transaction_screen.dart
@@ -93,7 +93,6 @@ class _DetailTransactionScreenState extends BaseState<DetailTransactionScreen>
                 SizedBox(height: 16),
                 if ((transaction?.description ?? '').isNotEmpty)
                   buildDescription(),
-
               ],
 
             ),
