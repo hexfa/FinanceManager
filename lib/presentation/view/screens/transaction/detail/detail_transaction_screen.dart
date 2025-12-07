5f7c6bdebe0f0cd1da1d73e6981e72dc0f78ee57
@hexfa <amir.dehdarian2009@gmail.com>
2025-12-07 22:15:44 +0300

    Clean code

--- lib/presentation/view/screens/transaction/detail/detail_transaction_screen.dart
+++ lib/presentation/view/screens/transaction/detail/detail_transaction_screen.dart
@@ -177,7 +177,6 @@ class _DetailTransactionScreenState extends BaseState<DetailTransactionScreen>
                         navigator.pop();
                       },
                     );
-
                   },
 
                 );
