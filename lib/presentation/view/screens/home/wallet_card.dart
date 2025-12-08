6d353e4728b79dd96df26ae95890b342805d6213
@hexfa <amir.dehdarian2009@gmail.com>
2025-12-08 14:12:27 +0300

    Clean

--- lib/presentation/view/screens/home/wallet_card.dart
+++ lib/presentation/view/screens/home/wallet_card.dart
@@ -7,7 +7,6 @@ import 'package:flutter/material.dart';
 class WalletCard extends BaseStatelessWidget {
   final double balance;
   final double income;
-
   final double expense;
 
   final String lastUpdate;
