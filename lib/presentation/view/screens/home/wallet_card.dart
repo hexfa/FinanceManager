7acbb1a62c49cc540379c41db4cd1504e2f4ff0f
@hexfa <amir.dehdarian2009@gmail.com>
2025-12-08 14:11:45 +0300

    Testing

--- lib/presentation/view/screens/home/wallet_card.dart
+++ lib/presentation/view/screens/home/wallet_card.dart
@@ -5,57 +5,111 @@ import 'package:finance_manager/presentation/view/widgets/currency_label.dart';
 import 'package:flutter/material.dart';
 
 class WalletCard extends BaseStatelessWidget {
+
   final double balance;
+
   final double income;
+
   final double expense;
+
   final String lastUpdate;
 
+
+
   const WalletCard({
+
     super.key,
+
     required this.balance,
+
     required this.income,
+
     required this.expense,
+
     required this.lastUpdate,
+
   });
 
+
+
   @override
+
   Widget build(BuildContext context) {
+
     return Padding(
+
       padding: const EdgeInsets.symmetric(horizontal: 16),
+
       child: Container(
+
         width: double.infinity,
+
         padding: const EdgeInsets.all(16),
+
         decoration: BoxDecoration(
+
           color: theme(context).colorScheme.primary,
+
           borderRadius: BorderRadius.circular(12),
+
           boxShadow: [
+
             BoxShadow(
+
               color: Colors.black.withValues(alpha: 0.2),
+
               blurRadius: 10,
+
               offset: const Offset(0, 4),
+
             ),
+
           ],
+
         ),
 
+
+
         child: Column(
+
           crossAxisAlignment: CrossAxisAlignment.center,
+
           children: [
+
             CurrencyLabel(icon: Icons.currency_exchange, balance: balance),
+
             const SizedBox(height: 8),
+
             Text(
+
               '${localization(context).updated} $lastUpdate',
+
               style: theme(context).textTheme.labelMedium?.copyWith(
+
                 color: theme(context).colorScheme.tertiaryContainer,
+
               ),
+
             ),
+
             const SizedBox(height: 16),
+
             IncomeExpenseSummary(
+
               income: ConvertString.formatCurrencyFromDouble(income),
+
               expense: ConvertString.formatCurrencyFromDouble(expense),
+
             ),
+
           ],
+
         ),
+
       ),
+
     );
+
   }
+
 }
