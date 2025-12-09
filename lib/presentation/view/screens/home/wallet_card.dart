f8d74d401be3fa51b1f46f8e8f753b31e716fd20
@hexfa <amir.dehdarian2009@gmail.com>
2025-12-09 11:38:15 +0300

    Clean

--- lib/presentation/view/screens/home/wallet_card.dart
+++ lib/presentation/view/screens/home/wallet_card.dart
@@ -21,7 +21,6 @@ class WalletCard extends BaseStatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-
       padding: const EdgeInsets.symmetric(horizontal: 16),
 
       child: Container(
