b7628d65d5eb6b096ef583abcfa86cb19787acec
@hexfa <amir.dehdarian2009@gmail.com>
2025-12-10 12:17:23 +0300

    Clean

--- lib/presentation/view/screens/home/wallet_card.dart
+++ lib/presentation/view/screens/home/wallet_card.dart
@@ -38,7 +38,6 @@ class WalletCard extends BaseStatelessWidget {
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
-
           children: [
 
             CurrencyLabel(icon: Icons.currency_exchange, balance: balance),
