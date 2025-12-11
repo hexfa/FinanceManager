7bd3d5418990de47750d4a97742cc24c695944c2
@hexfa <amir.dehdarian2009@gmail.com>
2025-12-11 17:50:20 +0300

    Clean

--- lib/presentation/view/screens/home/wallet_card.dart
+++ lib/presentation/view/screens/home/wallet_card.dart
@@ -42,7 +42,6 @@ class WalletCard extends BaseStatelessWidget {
             CurrencyLabel(icon: Icons.currency_exchange, balance: balance),
             const SizedBox(height: 8),
             Text(
-
               '${localization(context).updated} $lastUpdate',
 
               style: theme(context).textTheme.labelMedium?.copyWith(
