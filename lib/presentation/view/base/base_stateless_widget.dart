f2aeb3c4983eaff65bb6c70c8f773886365f0242
@hexfa <amir.dehdarian2009@gmail.com>
2025-12-15 13:33:47 +0300

    Clean code

--- lib/presentation/view/base/base_stateless_widget.dart
+++ lib/presentation/view/base/base_stateless_widget.dart
@@ -71,7 +71,6 @@ abstract class BaseStatelessWidget extends StatelessWidget {
 
   // Get bloc object
   TBloc getBloc<TBloc extends StateStreamableSource<Object?>>(
-
     BuildContext context,
 
       ) {
