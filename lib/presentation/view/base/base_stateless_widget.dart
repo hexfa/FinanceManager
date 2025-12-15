06ed24f4e837c1a6f932dee4f9ecb0474548b977
@hexfa <amir.dehdarian2009@gmail.com>
2025-12-15 13:33:29 +0300

    Clean code

--- lib/presentation/view/base/base_stateless_widget.dart
+++ lib/presentation/view/base/base_stateless_widget.dart
@@ -66,7 +66,6 @@ abstract class BaseStatelessWidget extends StatelessWidget {
         width: size,
         height: size,
         colorFilter:
-
             color != null ? ColorFilter.mode(color, BlendMode.srcIn) : null,
 
       );
