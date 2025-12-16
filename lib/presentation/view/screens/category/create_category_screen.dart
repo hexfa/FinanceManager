804ea4825c7bcc1de304db1f62a38c82a11eaa5e
@hexfa <amir.dehdarian2009@gmail.com>
2025-12-16 14:20:35 +0300

    Testing

--- lib/presentation/view/screens/category/create_category_screen.dart
+++ lib/presentation/view/screens/category/create_category_screen.dart
@@ -6,53 +6,92 @@ import 'package:finance_manager/presentation/view/widgets/textfield/custom_text_
 import 'package:flutter/material.dart';
 
 class CreateCategoryScreen extends StatefulWidget {
+
   const CreateCategoryScreen({super.key});
 
   @override
+
   State<CreateCategoryScreen> createState() => _CreateCategoryScreenState();
+
 }
 
 class _CreateCategoryScreenState extends BaseState<CreateCategoryScreen> {
+
   late TextEditingController nameController;
 
   @override
+
   void initState() {
+
     super.initState();
+
     nameController = TextEditingController();
 
     // nameController.addListener(() {
     //   getBloc<TransactionCubit>().updateTitle(titleController.text);
     // });
+
   }
 
+
   @override
+
   Widget build(BuildContext context) {
+
     return Scaffold(
+
       appBar: CustomAppBar(title: localization.createCategory),
+
       body: Padding(
+
         padding: const EdgeInsets.all(16),
+
         child: Column(
+
           mainAxisSize: MainAxisSize.max,
+
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
           children: [
+
             CustomTextField(
+
               prefixIcon: Icons.category_outlined,
+
               hint: '${localization.name} ...',
+
               controller: nameController,
+
             ),
+
             CustomButton(
+
               text: localization.create,
+
               onPressed: () async {
+
                 final name = nameController.text.trim();
+
                 if (name.isNotEmpty) {
+
                   await getBloc<CategoryCubit>().create(name);
+
                   navigator.pop();
+
                 }
+
               },
+
             ),
+
           ],
+
         ),
+
       ),
+
     );
+
   }
+
 }
