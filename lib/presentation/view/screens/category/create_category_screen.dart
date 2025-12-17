80ceca9a10048e241a94344f24c686e35964b711
@hexfa <amir.dehdarian2009@gmail.com>
2025-12-17 14:24:25 +0300

    Clean code

--- lib/presentation/view/screens/category/create_category_screen.dart
+++ lib/presentation/view/screens/category/create_category_screen.dart
@@ -27,7 +27,6 @@ class _CreateCategoryScreenState extends BaseState<CreateCategoryScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(title: localization.createCategory),
-
       body: Padding(
 
         padding: const EdgeInsets.all(16),
