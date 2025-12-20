def842d4c1e153fe1f90866d1d0fe23f79d37cca
@hexfa <amir.dehdarian2009@gmail.com>
2025-12-20 13:04:30 +0300

    Clean

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,6 @@ final RouteObserver<ModalRoute<void>> routeObserver = RouteObserver<ModalRoute<v
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   await Hive.initFlutter();
 
   Hive.registerAdapter(TransactionTypeAdapter());
