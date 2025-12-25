6c12684b73984c616b5cbe47bdcde648122c2a21
@hexfa <amir.dehdarian2009@gmail.com>
2025-12-25 14:46:50 +0300

    Clean

--- lib/presentation/navigation/app_router.dart
+++ lib/presentation/navigation/app_router.dart
@@ -19,7 +19,6 @@ import 'package:go_router/go_router.dart';
 
 class AppRouter {
   late final GoRouter router = GoRouter(
-
     initialLocation: RoutePath.homeRoute,
     routes: [
       GoRoute(
