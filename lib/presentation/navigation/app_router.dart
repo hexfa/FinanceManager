959e66ea5be4a9b86448af9424723350d29f11c3
@hexfa <amir.dehdarian2009@gmail.com>
2025-12-29 09:00:31 +0300

    Clean code

--- lib/presentation/navigation/app_router.dart
+++ lib/presentation/navigation/app_router.dart
@@ -68,7 +68,6 @@ class AppRouter {
               providers: [
                 BlocProvider(create: (_) => getIt<TransactionCubit>()),
                 BlocProvider(create: (_) => getIt<CategoryCubit>()..getAll()),
-
               ],
 
               child: UpdateTransactionScreen(
