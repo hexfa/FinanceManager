5d51008c4cd57d614d7a302aa415733429c93695
@hexfa <amir.dehdarian2009@gmail.com>
2026-01-04 10:22:26 +0300

    Clean code

--- lib/presentation/navigation/app_router.dart
+++ lib/presentation/navigation/app_router.dart
@@ -124,7 +124,6 @@ class AppRouter {
         const begin = Offset(1.0, 0.0);
         const end = Offset.zero;
         const curve = Curves.ease;
-
         var tween = Tween(
 
           begin: begin,
