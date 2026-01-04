d4f34fc029d65d31d87a43a63c1f3c8a94d61563
@hexfa <amir.dehdarian2009@gmail.com>
2026-01-04 10:21:51 +0300

    Clean code

--- lib/presentation/navigation/app_router.dart
+++ lib/presentation/navigation/app_router.dart
@@ -123,7 +123,6 @@ class AppRouter {
       transitionsBuilder: (context, animation, secondaryAnimation, child) {
         const begin = Offset(1.0, 0.0);
         const end = Offset.zero;
-
         const curve = Curves.ease;
 
 
