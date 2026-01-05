6ed6d2c5f8c2a22e94214bfc9a88421c3a17e1a7
@hexfa <amir.dehdarian2009@gmail.com>
2026-01-05 19:38:17 +0300

    test

--- lib/presentation/view/base/base_state.dart
+++ lib/presentation/view/base/base_state.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:finance_manager/l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
-
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:go_router/go_router.dart';
