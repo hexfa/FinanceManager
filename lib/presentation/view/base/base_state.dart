401e098ee95f419c577a45b2d1feacd4d3354cb5
@hexfa <amir.dehdarian2009@gmail.com>
2026-01-06 15:34:23 +0300

    Cleaning

--- lib/presentation/view/base/base_state.dart
+++ lib/presentation/view/base/base_state.dart
@@ -20,7 +20,6 @@ abstract class BaseState<T extends StatefulWidget> extends State<T> {
   }
 
   ThemeData get theme => Theme.of(context);
-
   // MediaQuery for accessing screen size, orientation, etc.
 
   MediaQueryData get mediaQuery => MediaQuery.of(context);
