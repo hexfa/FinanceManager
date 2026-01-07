998bbe0121566fbff022642daf09e8113727710f
@hexfa <amir.dehdarian2009@gmail.com>
2026-01-07 17:29:22 +0300

    Clean

--- lib/presentation/view/base/base_state.dart
+++ lib/presentation/view/base/base_state.dart
@@ -33,7 +33,6 @@ abstract class BaseState<T extends StatefulWidget> extends State<T> {
   // Common method to show a SnackBar with a message
   void showSnackBar(String message) {
     ScaffoldMessenger.of(
-
       context,
 
     ).showSnackBar(SnackBar(content: Text(message)));
