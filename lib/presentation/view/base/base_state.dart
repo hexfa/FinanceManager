c99473a48172794894c60bc0c20fd16ba454a12c
@hexfa <amir.dehdarian2009@gmail.com>
2026-01-10 14:43:31 +0300

    Clean

--- lib/presentation/view/base/base_state.dart
+++ lib/presentation/view/base/base_state.dart
@@ -46,7 +46,6 @@ abstract class BaseState<T extends StatefulWidget> extends State<T> {
   // Method to check if the platform is iOS
   bool get isIOS => Platform.isIOS;
 
-
   // Method to check if the platform is Android
 
   bool get isAndroid => Platform.isAndroid;
