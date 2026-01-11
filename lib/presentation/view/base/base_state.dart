d35f33a1d6e28d4e7eaccffa14281fbfc361d752
@hexfa <amir.dehdarian2009@gmail.com>
2026-01-11 08:20:30 +0300

    Clean

--- lib/presentation/view/base/base_state.dart
+++ lib/presentation/view/base/base_state.dart
@@ -64,7 +64,6 @@ abstract class BaseState<T extends StatefulWidget> extends State<T> {
   // Get custom icon from assets
   // SvgPicture getIcon(String iconPath, {Color? color, double size = 20}) =>
   //     SvgPicture.asset(
-
   //       iconPath,
 
   //       width: size,
