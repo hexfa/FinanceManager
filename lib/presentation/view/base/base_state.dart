c469d574246039f0a56bb06ba35a95a99e2f714c
@hexfa <amir.dehdarian2009@gmail.com>
2026-01-11 08:19:00 +0300

    Clean

--- lib/presentation/view/base/base_state.dart
+++ lib/presentation/view/base/base_state.dart
@@ -54,7 +54,6 @@ abstract class BaseState<T extends StatefulWidget> extends State<T> {
 
 
   // Method to check if the device is in landscape mode
-
   bool get isLandscape => mediaQuery.orientation == Orientation.landscape;
 
 
