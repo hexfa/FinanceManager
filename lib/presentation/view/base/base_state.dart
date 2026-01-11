9ed0b9c3e50ba4c3da518fc7ceb412170561ff83
@hexfa <amir.dehdarian2009@gmail.com>
2026-01-11 08:20:41 +0300

    Clean

--- lib/presentation/view/base/base_state.dart
+++ lib/presentation/view/base/base_state.dart
@@ -66,7 +66,6 @@ abstract class BaseState<T extends StatefulWidget> extends State<T> {
   //     SvgPicture.asset(
   //       iconPath,
   //       width: size,
-
   //       height: size,
 
   //       colorFilter: color != null
