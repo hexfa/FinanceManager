4d71de9330901da2b79f3165031f872bbd75351c
@hexfa <amir.dehdarian2009@gmail.com>
2026-01-11 08:20:18 +0300

    Clean

--- lib/presentation/view/base/base_state.dart
+++ lib/presentation/view/base/base_state.dart
@@ -62,7 +62,6 @@ abstract class BaseState<T extends StatefulWidget> extends State<T> {
   double get screenHeight => mediaQuery.size.height;
 
   // Get custom icon from assets
-
   // SvgPicture getIcon(String iconPath, {Color? color, double size = 20}) =>
 
   //     SvgPicture.asset(
