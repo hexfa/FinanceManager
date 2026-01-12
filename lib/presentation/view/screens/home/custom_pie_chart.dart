31b14de94b1e79bdd3f0c2cbfec9cb6fa94c359f
@hexfa <amir.dehdarian2009@gmail.com>
2026-01-12 15:44:28 +0300

    Cleaning

--- lib/presentation/view/screens/home/custom_pie_chart.dart
+++ lib/presentation/view/screens/home/custom_pie_chart.dart
@@ -2,7 +2,6 @@ import 'package:finance_manager/core/utils/convert_string.dart';
 import 'package:finance_manager/data/models/transaction_chart_data.dart';
 import 'package:finance_manager/presentation/view/base/base_stateless_widget.dart';
 import 'package:fl_chart/fl_chart.dart';
-
 import 'package:flutter/material.dart';
 
 
