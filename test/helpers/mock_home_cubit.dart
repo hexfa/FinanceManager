eea3fdcad67e0c1bd2b645c3f1739848e8e4f9d5
@hexfa <amir.dehdarian2009@gmail.com>
2026-01-13 11:37:46 +0300

    Cleaning

--- test/helpers/mock_home_cubit.dart
+++ test/helpers/mock_home_cubit.dart
@@ -1,4 +1,4 @@
-import 'package:finance_manager/presentation/bloc/home/home_cubit.dart';
+
 import 'package:finance_manager/presentation/bloc/home/home_state.dart';
 
 // class MockHomeCubit extends MockCubit<HomeState> implements HomeCubit {}
