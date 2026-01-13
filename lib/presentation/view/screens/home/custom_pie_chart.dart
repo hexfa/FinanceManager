c5d1f1b1ab55c8130bbf5459ede3529fcf8aaa5c
@hexfa <amir.dehdarian2009@gmail.com>
2026-01-13 11:35:24 +0300

    Cleaning

--- lib/presentation/view/screens/home/custom_pie_chart.dart
+++ lib/presentation/view/screens/home/custom_pie_chart.dart
@@ -25,7 +25,6 @@ class CustomPieChart extends BaseStatelessWidget {
                       ),
                     ),
                   )
-
                   .toList(),
 
           sectionsSpace: 2,
