a75fc65d77952151f9eb763694cc258d5fb661c6
@hexfa <amir.dehdarian2009@gmail.com>
2026-01-13 11:34:40 +0300

    Cleaning

--- lib/presentation/view/screens/home/custom_pie_chart.dart
+++ lib/presentation/view/screens/home/custom_pie_chart.dart
@@ -21,7 +21,6 @@ class CustomPieChart extends BaseStatelessWidget {
                       title:
                           '${data.title}\n${ConvertString.formatCurrencyFromDouble(data.percentage)}%',
                       titleStyle: theme(context).textTheme.labelSmall?.copyWith(
-
                         color: theme(context).colorScheme.surface,
 
                       ),
