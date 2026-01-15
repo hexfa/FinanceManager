a8f18f58f7641d01e4a71c0121ed6afb5f1a6278
@hexfa <amir.dehdarian2009@gmail.com>
2026-01-15 19:14:22 +0300

    Clean

--- lib/presentation/view/widgets/dropdown/custom_drop_down.dart
+++ lib/presentation/view/widgets/dropdown/custom_drop_down.dart
@@ -73,7 +73,6 @@ class CustomDropdown<T> extends BaseStatelessWidget {
 
 
           items.map((item) {
-
              final itemLabel = itemLabelBuilder?.call(item) ?? item.toString();
             return DropdownMenuItem<T>(value: item, child: Text(itemLabel));
           }).toList(),
