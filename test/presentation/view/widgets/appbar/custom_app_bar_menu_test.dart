a44e9d681ab1cd2e05966aa424b69511a5c1e8a0
@hexfa <amir.dehdarian2009@gmail.com>
2026-01-17 19:02:00 +0300

    Cleaning

--- test/presentation/view/widgets/appbar/custom_app_bar_menu_test.dart
+++ test/presentation/view/widgets/appbar/custom_app_bar_menu_test.dart
@@ -1,5 +1,4 @@
 import 'package:finance_manager/data/models/app_bar_menu.dart';
-
 import 'package:finance_manager/presentation/view/widgets/appbar/custom_app_bar_menu.dart';
 
 import 'package:flutter/material.dart';
