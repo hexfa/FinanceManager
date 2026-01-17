decc88288b3b8af6e5b27f821202e83ac6340586
@hexfa <amir.dehdarian2009@gmail.com>
2026-01-17 19:02:47 +0300

    Cleaning

--- test/presentation/view/widgets/appbar/custom_app_bar_menu_test.dart
+++ test/presentation/view/widgets/appbar/custom_app_bar_menu_test.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import '../../../../helpers/build_testable_widget.dart';
 
-
 void main() {
 
   testWidgets('verify more_vert icon is displayed in CustomAppBarMenu', (
