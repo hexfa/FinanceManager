1f6ad8fd96053b0642d125148d1bba4e25ee9de7
@hexfa <amir.dehdarian2009@gmail.com>
2026-01-17 18:51:12 +0300

    Clean

--- lib/presentation/view/widgets/dropdown/custom_drop_down.dart
+++ lib/presentation/view/widgets/dropdown/custom_drop_down.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:finance_manager/presentation/view/widgets/app_decoration.dart';
 import 'package:finance_manager/presentation/view/base/base_stateless_widget.dart';
@@ -10,6 +9,7 @@ class CustomDropdown<T> extends BaseStatelessWidget {
   final String Function(T)? itemLabelBuilder;
   final IconData Function(T)? iconBuilder;
   final String? hint;
+
   const CustomDropdown({
     super.key,
     required this.items,
@@ -19,20 +19,22 @@ class CustomDropdown<T> extends BaseStatelessWidget {
     this.iconBuilder,
     this.hint,
   });
+
   @override
   Widget build(BuildContext context) {
     final Widget? dynamicIcon =
         (value != null && iconBuilder != null)
             ? Icon(
-          iconBuilder!(value as T),
+              iconBuilder!(value as T),
               color: theme(context).colorScheme.onSurface,
-          // size: 20,
-        ) : null;
+              // size: 20,
+            )
+            : null;
     return DropdownButtonFormField<T>(
       initialValue: value,
       items:
-      items.map((item) {
-             final itemLabel = itemLabelBuilder?.call(item) ?? item.toString();
+          items.map((item) {
+            final itemLabel = itemLabelBuilder?.call(item) ?? item.toString();
             return DropdownMenuItem<T>(value: item, child: Text(itemLabel));
           }).toList(),
       onChanged: onChanged,
