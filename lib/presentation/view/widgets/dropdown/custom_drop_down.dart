f776b18915b3874684ed94613112664eabc6672c
@hexfa <amir.dehdarian2009@gmail.com>
2026-01-17 18:49:18 +0300

    Clean

--- lib/presentation/view/widgets/dropdown/custom_drop_down.dart
+++ lib/presentation/view/widgets/dropdown/custom_drop_down.dart
@@ -16,7 +16,6 @@ class CustomDropdown<T> extends BaseStatelessWidget {
   final ValueChanged<T?> onChanged;
 
   final String Function(T)? itemLabelBuilder;
-
   final IconData Function(T)? iconBuilder;
   final String? hint;
   const CustomDropdown({
