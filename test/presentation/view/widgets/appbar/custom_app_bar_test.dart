22eb7d892d67a931344147d09849a2f2652cc4a1
@hexfa <amir.dehdarian2009@gmail.com>
2026-01-19 14:24:22 +0300

    Clean code

--- test/presentation/view/widgets/appbar/custom_app_bar_test.dart
+++ test/presentation/view/widgets/appbar/custom_app_bar_test.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-
   testWidgets('verify title is rendered correctly in CustomAppBar', (
 
     tester,
