f5390eb66e90950af9d038ac1e55749ad2750494
@hexfa <amir.dehdarian2009@gmail.com>
2026-01-19 14:27:08 +0300

    Clean code

--- test/presentation/view/widgets/appbar/custom_app_bar_test.dart
+++ test/presentation/view/widgets/appbar/custom_app_bar_test.dart
@@ -18,7 +18,6 @@ void main() {
     const leadingIcon = Icon(Icons.menu);
     await tester.pumpWidget(
       MaterialApp(
-
         home: Scaffold(
 
           appBar: CustomAppBar(title: 'Test', leading: leadingIcon),
