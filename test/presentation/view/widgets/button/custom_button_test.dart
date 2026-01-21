6dbcb2583744e2d9fde5ffc9b0e540978ac2cbcf
@hexfa <amir.dehdarian2009@gmail.com>
2026-01-21 20:28:47 +0300

    Clean code

--- test/presentation/view/widgets/button/custom_button_test.dart
+++ test/presentation/view/widgets/button/custom_button_test.dart
@@ -41,7 +41,6 @@ void main() {
   });
   testWidgets('should have correct style', (tester) async {
     await tester.pumpWidget(
-
       MaterialApp(
 
         home: Scaffold(body: CustomButton(text: 'Styled', onPressed: () {})),
