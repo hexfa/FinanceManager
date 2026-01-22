6f4b7f501690de16158ebcebdcfa1e8811cd616b
@hexfa <amir.dehdarian2009@gmail.com>
2026-01-22 15:53:36 +0300

    Clean

--- test/presentation/view/widgets/choicechip/custom_type_selector_test.dart
+++ test/presentation/view/widgets/choicechip/custom_type_selector_test.dart
@@ -1,5 +1,4 @@
 import 'package:finance_manager/core/utils/transaction_type.dart';
-
 import 'package:finance_manager/presentation/view/widgets/choicechip/custom_type_selector.dart';
 
 import 'package:flutter/material.dart';
