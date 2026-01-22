e2729b6103d0340b2176ba785acb994f7bbffe54
@hexfa <amir.dehdarian2009@gmail.com>
2026-01-22 15:54:14 +0300

    Clean

--- test/presentation/view/widgets/choicechip/custom_type_selector_test.dart
+++ test/presentation/view/widgets/choicechip/custom_type_selector_test.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-
   testWidgets('displays both income and expense chips', (tester) async {
 
     await tester.pumpWidget(
