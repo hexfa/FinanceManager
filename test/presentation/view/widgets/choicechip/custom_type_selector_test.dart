4a7d78d5780bbeb384d278233cfb50b2d70681a3
@hexfa <amir.dehdarian2009@gmail.com>
2026-01-25 11:32:23 +0300

    Clean

--- test/presentation/view/widgets/choicechip/custom_type_selector_test.dart
+++ test/presentation/view/widgets/choicechip/custom_type_selector_test.dart
@@ -42,7 +42,6 @@ void main() {
     );
     await tester.tap(find.text('income'));
     expect(selected, TransactionType.income);
-
     await tester.tap(find.text('expense'));
 
     expect(selected, TransactionType.expense);
