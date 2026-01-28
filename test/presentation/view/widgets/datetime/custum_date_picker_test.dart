1a4d0fb23984b0b5fd7d330128e601069b04c230
@hexfa <amir.dehdarian2009@gmail.com>
2026-01-28 22:59:59 +0300

    Clean code

--- test/presentation/view/widgets/datetime/custum_date_picker_test.dart
+++ test/presentation/view/widgets/datetime/custum_date_picker_test.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 
-
 void main() {
 
   testWidgets('displays current datetime if no initial value is provided', (
