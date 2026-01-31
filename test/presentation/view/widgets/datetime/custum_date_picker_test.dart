017bc891731a4cda0b1be44244361a2c5870e863
@hexfa <amir.dehdarian2009@gmail.com>
2026-01-31 07:28:15 +0300

    Clean code

--- test/presentation/view/widgets/datetime/custum_date_picker_test.dart
+++ test/presentation/view/widgets/datetime/custum_date_picker_test.dart
@@ -14,7 +14,6 @@ void main() {
   testWidgets('displays initial datetime if provided', (tester) async {
     final initial = DateTime(2023, 1, 1, 10, 30);
 
-
     await tester.pumpWidget(
 
       MaterialApp(
