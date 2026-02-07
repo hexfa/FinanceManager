fc31954ae295bb296dd54a3c86cad8d0d3c871d7
@hexfa <amir.dehdarian2009@gmail.com>
2026-02-07 16:34:54 +0300

    Clean

--- test/presentation/view/widgets/datetime/custum_date_picker_test.dart
+++ test/presentation/view/widgets/datetime/custum_date_picker_test.dart
@@ -39,7 +39,6 @@ void main() {
   });
   testWidgets('renders layout with Row and Expanded text', (tester) async {
     await tester.pumpWidget(
-
       MaterialApp(home: DateTimePickerContainer(onDateTimeSelected: (_) {})),
 
     );
