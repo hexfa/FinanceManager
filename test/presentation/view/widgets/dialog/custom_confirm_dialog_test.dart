7c82b0809d4b6640ea3c8bdf2f6925683e6b6ef5
@hexfa <amir.dehdarian2009@gmail.com>
2026-02-08 16:29:59 +0300

    Cleaning

--- test/presentation/view/widgets/dialog/custom_confirm_dialog_test.dart
+++ test/presentation/view/widgets/dialog/custom_confirm_dialog_test.dart
@@ -1,6 +1,5 @@
 import 'package:finance_manager/presentation/view/widgets/dialog/custom_confirm_dialog.dart';
 import 'package:flutter/material.dart';
-
 import 'package:flutter_test/flutter_test.dart';
 
 
