779fc3dfb492650e5687df67b4d8da083df1f32c
@hexfa <amir.dehdarian2009@gmail.com>
2026-02-11 07:18:35 +0300

    Cleaning

--- test/presentation/view/widgets/dialog/custom_confirm_dialog_test.dart
+++ test/presentation/view/widgets/dialog/custom_confirm_dialog_test.dart
@@ -28,7 +28,6 @@ void main() {
     );
     await tester.tap(find.text('Show Dialog'));
     await tester.pumpAndSettle();
-
     expect(find.text('Delete Item'), findsOneWidget);
 
     expect(find.text('Are you sure?'), findsOneWidget);
