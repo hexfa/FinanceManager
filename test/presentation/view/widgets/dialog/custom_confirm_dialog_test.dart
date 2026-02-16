5279a35097ca65c6472d8affdd5605f737e17aa8
@hexfa <amir.dehdarian2009@gmail.com>
2026-02-16 15:49:52 +0300

    Cleaning

--- test/presentation/view/widgets/dialog/custom_confirm_dialog_test.dart
+++ test/presentation/view/widgets/dialog/custom_confirm_dialog_test.dart
@@ -4,6 +4,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 
 void main() {
+
   testWidgets('displays title and content text correctly', (tester) async {
     await tester.pumpWidget(
       MaterialApp(
