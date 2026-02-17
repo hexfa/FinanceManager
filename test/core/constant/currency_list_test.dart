84321ff21bc16583fc5f95d117a0c5e30b4ba2dc
@hexfa <amir.dehdarian2009@gmail.com>
2026-02-17 21:05:33 +0300

    Clean code

--- test/core/constant/currency_list_test.dart
+++ test/core/constant/currency_list_test.dart
@@ -1,6 +1,5 @@
 import 'package:finance_manager/core/constants/currency.dart';
 import 'package:finance_manager/data/models/currency.dart';
-
 import 'package:flutter/material.dart';
 
 import 'package:flutter_test/flutter_test.dart';
