751adfa65f95a8f40edda5ff256302b8cf352cea
@hexfa <amir.dehdarian2009@gmail.com>
2026-02-17 21:06:31 +0300

    Clean code

--- test/core/constant/currency_list_test.dart
+++ test/core/constant/currency_list_test.dart
@@ -6,7 +6,6 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   group('CurrencyCodeExtension', () {
     test('returns correct Currency for valid code', () {
-
       expect('EUR'.toCurrency().name, 'Euro');
 
       expect('JPY'.toCurrency().name, 'Japanese Yen');
