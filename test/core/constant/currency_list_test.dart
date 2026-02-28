dba1b53423d78b111fa84b2aa289bd45392e62d5
@hexfa <amir.dehdarian2009@gmail.com>
2026-02-28 18:41:10 +0300

    Clean code

--- test/core/constant/currency_list_test.dart
+++ test/core/constant/currency_list_test.dart
@@ -3,6 +3,7 @@ import 'package:finance_manager/data/models/currency.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+//test
 void main() {
   group('CurrencyCodeExtension', () {
     test('returns correct Currency for valid code', () {
