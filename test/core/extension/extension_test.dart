9ef51c7862482a8c75855ffe6d2e9897939b17ee
@hexfa <amir.dehdarian2009@gmail.com>
2026-03-01 20:27:32 +0300

    Cleaning

--- test/core/extension/extension_test.dart
+++ test/core/extension/extension_test.dart
@@ -1,6 +1,5 @@
 import 'package:finance_manager/core/extension/extension.dart';
 import 'package:finance_manager/core/utils/transaction_type.dart';
-
 import 'package:finance_manager/data/models/category.dart';
 
 import 'package:finance_manager/data/models/transaction.dart';
