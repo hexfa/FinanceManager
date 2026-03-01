171ae9d72dbfe15f8fabb7544ad975dca9957e7e
@hexfa <amir.dehdarian2009@gmail.com>
2026-03-01 20:28:00 +0300

    Cleaning

--- test/core/extension/extension_test.dart
+++ test/core/extension/extension_test.dart
@@ -2,7 +2,6 @@ import 'package:finance_manager/core/extension/extension.dart';
 import 'package:finance_manager/core/utils/transaction_type.dart';
 import 'package:finance_manager/data/models/category.dart';
 import 'package:finance_manager/data/models/transaction.dart';
-
 import 'package:finance_manager/domain/entities/transaction_entity.dart';
 
 import 'package:flutter_test/flutter_test.dart';
