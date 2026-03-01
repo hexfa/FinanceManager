f8e1e0b21d29bb01b4b8f11b0e6a438429665ea9
@hexfa <amir.dehdarian2009@gmail.com>
2026-03-01 20:26:07 +0300

    Testing

--- test/core/extension/extension_test.dart
+++ test/core/extension/extension_test.dart
@@ -1,47 +1,96 @@
+
 import 'package:finance_manager/core/extension/extension.dart';
+
 import 'package:finance_manager/core/utils/transaction_type.dart';
+
 import 'package:finance_manager/data/models/category.dart';
+
 import 'package:finance_manager/data/models/transaction.dart';
+
 import 'package:finance_manager/domain/entities/transaction_entity.dart';
+
 import 'package:flutter_test/flutter_test.dart';
 
+
+
 void main() {
+
   test('TransactionEntity toModel returns correct Transaction', () {
+
     final entity = TransactionEntity(
+
       title: 'Lunch',
+
       description: 'Lunch Description',
+
       amount: 50.0,
+
       type: TransactionType.expense,
+
       category: Category(id: 1, name: 'name'),
+
       date: DateTime(2024, 1, 1),
+
     );
+
+
 
     final model = entity.toModel();
 
+
+
+
     expect(model.title, entity.title);
+
     expect(model.amount, entity.amount);
+
     expect(model.type, entity.type);
+
     expect(model.category, entity.category);
+
     expect(model.date, entity.date);
+
   });
 
+
+
   test('Transaction toEntity returns correct TransactionEntity', () {
+
     final model = Transaction(
+
       id: 1,
+
       title: 'Lunch',
+
       description: 'Lunch Description',
+
       amount: 50.0,
+
       type: TransactionType.expense,
+
       category: Category(id: 1, name: 'name'),
+
       date: DateTime(2024, 1, 1),
+
     );
+
+
 
     final entity = model.toEntity();
 
+
+
     expect(entity.title, model.title);
+
     expect(entity.amount, model.amount);
+
     expect(entity.type, model.type);
+
     expect(entity.category, model.category);
+
     expect(entity.date, model.date);
+
   });
+
 }
+
