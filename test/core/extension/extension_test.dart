a445d7a282a3522c12ac8c64c00e95324c8a1142
@hexfa <amir.dehdarian2009@gmail.com>
2026-03-01 20:28:40 +0300

    Cleaning

--- test/core/extension/extension_test.dart
+++ test/core/extension/extension_test.dart
@@ -7,7 +7,6 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   test('TransactionEntity toModel returns correct Transaction', () {
     final entity = TransactionEntity(
-
       title: 'Lunch',
 
       description: 'Lunch Description',
