1957da2afe49271cbd514ab094356433888246a7
@hexfa <amir.dehdarian2009@gmail.com>
2026-03-02 16:40:28 +0300

    Cleaning

--- test/core/extension/extension_test.dart
+++ test/core/extension/extension_test.dart
@@ -17,7 +17,6 @@ void main() {
     final model = entity.toModel();
     expect(model.title, entity.title);
     expect(model.amount, entity.amount);
-
     expect(model.type, entity.type);
 
     expect(model.category, entity.category);
