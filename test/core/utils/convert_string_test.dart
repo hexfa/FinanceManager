c3593329c201a63e3684938ab905e51efd6a5dad
@hexfa <amir.dehdarian2009@gmail.com>
2026-03-07 18:16:02 +0300

    Clean code

--- test/core/utils/convert_string_test.dart
+++ test/core/utils/convert_string_test.dart
@@ -3,7 +3,6 @@ import 'package:finance_manager/core/utils/convert_string.dart';
 
 void main() {
   group('ConvertString', () {
-
     test('formatDate formats DateTime correctly', () {
 
       final date = DateTime(2024, 5, 15);
