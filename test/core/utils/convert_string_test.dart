1ba142ee70016073af7faba48e71c14003c9fb4b
@hexfa <amir.dehdarian2009@gmail.com>
2026-03-08 16:53:01 +0300

    Clean code

--- test/core/utils/convert_string_test.dart
+++ test/core/utils/convert_string_test.dart
@@ -21,7 +21,6 @@ void main() {
     test('formatCurrencyFromDouble formats double values with commas', () {
       expect(ConvertString.formatCurrencyFromDouble(1000), '1,000');
       expect(
-
         ConvertString.formatCurrencyFromDouble(1234567.89),
 
         '1,234,568',
