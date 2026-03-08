38762289c1d2c9182ee61439337208788dcf4065
@hexfa <amir.dehdarian2009@gmail.com>
2026-03-08 16:53:59 +0300

    Clean code

--- test/core/utils/convert_string_test.dart
+++ test/core/utils/convert_string_test.dart
@@ -26,7 +26,6 @@ void main() {
       ); // rounded
       expect(ConvertString.formatCurrencyFromDouble(0), '0');
     });
-
     test('toDouble parses numeric strings with commas to double', () {
 
       expect(ConvertString.toDouble('1,000'), 1000.0);
