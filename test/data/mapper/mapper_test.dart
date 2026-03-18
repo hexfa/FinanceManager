804054564c3e69070406375dfee14a7843f2b0e3
@hexfa <amir.dehdarian2009@gmail.com>
2026-03-19 01:14:46 +0300

    Clean code

--- test/data/mapper/mapper_test.dart
+++ test/data/mapper/mapper_test.dart
@@ -5,7 +5,6 @@ import 'package:finance_manager/domain/entities/transaction_entity.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:finance_manager/core/utils/transaction_type.dart';
 
-
 void main() {
 
   test(
