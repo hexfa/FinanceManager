7ceb62f13d0b4b2e87217c27906f2f974c60a601
@hexfa <amir.dehdarian2009@gmail.com>
2026-03-19 01:16:52 +0300

    Clean code

--- test/data/mapper/mapper_test.dart
+++ test/data/mapper/mapper_test.dart
@@ -17,7 +17,6 @@ void main() {
         ),
       ];
       final models = TransactionMapper.toModelList(entities);
-
       expect(models.length, entities.length);
 
       expect(models.first.title, entities.first.title);
