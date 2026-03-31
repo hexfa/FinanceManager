e24bbab1f8e0cad74e8dfb7392521cc844973d9e
@hexfa <amir.dehdarian2009@gmail.com>
2026-03-31 16:03:36 +0300

    Test

--- test/data/repositories/setting_repository_imp_test.dart
+++ test/data/repositories/setting_repository_imp_test.dart
@@ -2,7 +2,6 @@ import 'package:finance_manager/data/repositories/setting_repository_imp.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:finance_manager/core/utils/configuration_key.dart';
 import 'package:finance_manager/data/datasource/sharepref_data_source.dart';
-
 import 'package:finance_manager/data/service/currency_service.dart';
 
 import 'package:mockito/annotations.dart';
