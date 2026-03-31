1d43007b4fb8a95b4b429901b3088408245c0fa5
@hexfa <amir.dehdarian2009@gmail.com>
2026-03-31 16:04:32 +0300

    Test

--- test/data/repositories/setting_repository_imp_test.dart
+++ test/data/repositories/setting_repository_imp_test.dart
@@ -5,7 +5,6 @@ import 'package:finance_manager/data/datasource/sharepref_data_source.dart';
 import 'package:finance_manager/data/service/currency_service.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
-
 import 'setting_repository_imp_test.mocks.dart';
 
 
