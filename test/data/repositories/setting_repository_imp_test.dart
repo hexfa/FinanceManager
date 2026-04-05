f5e23953618d2a9f65b3c25b12c1a448d6efdcab
@hexfa <amir.dehdarian2009@gmail.com>
2026-04-05 12:44:59 +0300

    Clean code

--- test/data/repositories/setting_repository_imp_test.dart
+++ test/data/repositories/setting_repository_imp_test.dart
@@ -22,7 +22,6 @@ void main() {
   });
   test('getCurrency returns saved value if present', () {
     when(mockDataSource.getString(ConfigurationKey.currency)).thenReturn('EUR');
-
     final result = repository.getCurrency();
 
 
