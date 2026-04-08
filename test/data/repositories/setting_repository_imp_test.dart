337617402488eac2a0a1f1b88dd258dd65a99324
@hexfa <amir.dehdarian2009@gmail.com>
2026-04-08 13:38:34 +0300

    Clean code

--- test/data/repositories/setting_repository_imp_test.dart
+++ test/data/repositories/setting_repository_imp_test.dart
@@ -43,7 +43,6 @@ void main() {
   });
   test('setDarkTheme stores the theme preference', () async {
     const isDark = true;
-
     when(
 
       mockDataSource.setBool(ConfigurationKey.darkTheme, isDark),
