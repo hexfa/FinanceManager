d53acab0fa1de3ffaedde554a0eab599e5dc6f3c
@hexfa <amir.dehdarian2009@gmail.com>
2026-04-09 18:19:31 +0300

    Clean code

--- test/data/repositories/setting_repository_imp_test.dart
+++ test/data/repositories/setting_repository_imp_test.dart
@@ -51,7 +51,6 @@ void main() {
       mockDataSource.setBool(ConfigurationKey.darkTheme, isDark),
     ).called(1);
   });
-
   test('isDarkTheme returns saved value when set', () {
 
     when(mockDataSource.getBool(ConfigurationKey.darkTheme)).thenReturn(true);
