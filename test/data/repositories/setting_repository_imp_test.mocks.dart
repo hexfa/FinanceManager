d2cf55d909cc9da67641329b55747e37a15c1e87
@hexfa <amir.dehdarian2009@gmail.com>
2026-04-12 10:38:05 +0300

    Test

--- test/data/repositories/setting_repository_imp_test.mocks.dart
+++ test/data/repositories/setting_repository_imp_test.mocks.dart
@@ -5,11 +5,18 @@
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'dart:async' as _i4;
 
+
 import 'package:finance_manager/data/datasource/sharepref_data_source.dart'
+
     as _i3;
+
+
 import 'package:finance_manager/data/service/currency_service.dart' as _i5;
+
 import 'package:mockito/mockito.dart' as _i1;
+
 import 'package:mockito/src/dummies.dart' as _i6;
+
 import 'package:shared_preferences/shared_preferences.dart' as _i2;
 
 // ignore_for_file: type=lint
