5a46079c912f3becfc7576f752a0524fa68becd1
@hexfa <amir.dehdarian2009@gmail.com>
2026-04-12 10:39:35 +0300

    Cleaning

--- test/data/repositories/setting_repository_imp_test.mocks.dart
+++ test/data/repositories/setting_repository_imp_test.mocks.dart
@@ -26,7 +26,6 @@ import 'package:shared_preferences/shared_preferences.dart' as _i2;
 
 
 class _FakeSharedPreferences_0 extends _i1.SmartFake
-
     implements _i2.SharedPreferences {
 
   _FakeSharedPreferences_0(
