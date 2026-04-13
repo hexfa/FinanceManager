e06838011a9653e395a06fb275c78a57f2f5c74c
@hexfa <amir.dehdarian2009@gmail.com>
2026-04-13 16:22:59 +0300

    Cleaning

--- test/data/repositories/setting_repository_imp_test.mocks.dart
+++ test/data/repositories/setting_repository_imp_test.mocks.dart
@@ -39,7 +39,6 @@ class _FakeSharedPreferences_0 extends _i1.SmartFake
 /// See the documentation for Mockito's code generation for more information.
 class MockShareprefDataSource extends _i1.Mock
     implements _i3.ShareprefDataSource {
-
   MockShareprefDataSource() {
 
     _i1.throwOnMissingStub(this);
