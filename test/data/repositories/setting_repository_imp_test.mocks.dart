5ef26785597322141cdc248a24a31a471711f5d5
@hexfa <amir.dehdarian2009@gmail.com>
2026-04-14 20:15:51 +0300

    Cleaning

--- test/data/repositories/setting_repository_imp_test.mocks.dart
+++ test/data/repositories/setting_repository_imp_test.mocks.dart
@@ -45,7 +45,6 @@ class MockShareprefDataSource extends _i1.Mock
   @override
   _i2.SharedPreferences get prefs => (super.noSuchMethod(
         Invocation.getter(#prefs),
-
         returnValue: _FakeSharedPreferences_0(
 
           this,
