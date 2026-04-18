300f06eaf1b99cf9cd1475277ff7feb719d907c8
@hexfa <amir.dehdarian2009@gmail.com>
2026-04-18 14:34:02 +0300

    Cleaning

--- test/data/repositories/setting_repository_imp_test.mocks.dart
+++ test/data/repositories/setting_repository_imp_test.mocks.dart
@@ -68,7 +68,6 @@ class MockShareprefDataSource extends _i1.Mock
         returnValueForMissingStub: _i4.Future<void>.value(),
       ) as _i4.Future<void>);
 
-
   @override
 
   bool? getBool(String? key) => (super.noSuchMethod(Invocation.method(
