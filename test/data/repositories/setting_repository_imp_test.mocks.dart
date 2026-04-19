cf3c77c9edb5a2b421cc76a712d9d20450c75bd5
@hexfa <amir.dehdarian2009@gmail.com>
2026-04-19 11:54:58 +0300

    Cleaning

--- test/data/repositories/setting_repository_imp_test.mocks.dart
+++ test/data/repositories/setting_repository_imp_test.mocks.dart
@@ -89,7 +89,6 @@ class MockShareprefDataSource extends _i1.Mock
 }
 /// A class which mocks [CurrencyService].
 /// See the documentation for Mockito's code generation for more information.
-
 class MockCurrencyService extends _i1.Mock implements _i5.CurrencyService {
 
   MockCurrencyService() {
