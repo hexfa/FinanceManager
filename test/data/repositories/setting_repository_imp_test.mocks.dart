bbff63ad261e72a0a7d3ec1b30d4464ba2917b3c
@hexfa <amir.dehdarian2009@gmail.com>
2026-04-19 11:54:34 +0300

    Cleaning

--- test/data/repositories/setting_repository_imp_test.mocks.dart
+++ test/data/repositories/setting_repository_imp_test.mocks.dart
@@ -87,7 +87,6 @@ class MockShareprefDataSource extends _i1.Mock
         returnValueForMissingStub: _i4.Future<void>.value(),
       ) as _i4.Future<void>);
 }
-
 /// A class which mocks [CurrencyService].
 
 ///
