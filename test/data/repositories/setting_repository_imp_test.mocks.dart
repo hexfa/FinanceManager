1601e6ee6adc4a009bd5ac59a57cf4edf21fa5a2
@hexfa <amir.dehdarian2009@gmail.com>
2026-04-20 10:42:03 +0300

    Cleaning

--- test/data/repositories/setting_repository_imp_test.mocks.dart
+++ test/data/repositories/setting_repository_imp_test.mocks.dart
@@ -97,7 +97,6 @@ class MockCurrencyService extends _i1.Mock implements _i5.CurrencyService {
   String get currencyCode => (super.noSuchMethod(
         Invocation.getter(#currencyCode),
         returnValue: _i6.dummyValue<String>(
-
           this,
 
           Invocation.getter(#currencyCode),
