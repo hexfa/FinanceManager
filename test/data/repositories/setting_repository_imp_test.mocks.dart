45c45487b20295f65e9a02c249ed308ce277723d
@hexfa <amir.dehdarian2009@gmail.com>
2026-04-21 18:57:54 +0300

    Cleaning

--- test/data/repositories/setting_repository_imp_test.mocks.dart
+++ test/data/repositories/setting_repository_imp_test.mocks.dart
@@ -117,7 +117,6 @@ class MockCurrencyService extends _i1.Mock implements _i5.CurrencyService {
         ),
         returnValueForMissingStub: null,
       );
-
   @override
 
   _i4.Future<void> notifyCurrencyChanged() => (super.noSuchMethod(
