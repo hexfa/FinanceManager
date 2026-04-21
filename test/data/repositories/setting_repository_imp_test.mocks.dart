f2afce2ac5ef8c0b745c8ebcb7b8a2889aae08ca
@hexfa <amir.dehdarian2009@gmail.com>
2026-04-21 18:58:24 +0300

    Cleaning

--- test/data/repositories/setting_repository_imp_test.mocks.dart
+++ test/data/repositories/setting_repository_imp_test.mocks.dart
@@ -121,7 +121,6 @@ class MockCurrencyService extends _i1.Mock implements _i5.CurrencyService {
   _i4.Future<void> notifyCurrencyChanged() => (super.noSuchMethod(
         Invocation.method(
           #notifyCurrencyChanged,
-
           [],
 
         ),
