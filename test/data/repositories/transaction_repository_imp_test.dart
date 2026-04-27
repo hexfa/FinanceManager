0341a515051a470b2a5f0239a78518322a798324
@hexfa <amir.dehdarian2009@gmail.com>
2026-04-27 17:44:45 +0300

    Clean code

--- test/data/repositories/transaction_repository_imp_test.dart
+++ test/data/repositories/transaction_repository_imp_test.dart
@@ -33,7 +33,6 @@ void main() {
         mockDataSource.getAll(),
       ).thenAnswer((_) async => [testTransactionEntity]);
 
-
       // Act
 
       final result = await repository.getAllTransactions();
