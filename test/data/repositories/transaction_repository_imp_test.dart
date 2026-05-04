546d3dbfae79fec726a70cacd1214992bf7403d0
@hexfa <amir.dehdarian2009@gmail.com>
2026-05-04 20:37:25 +0300

    Clean code

--- test/data/repositories/transaction_repository_imp_test.dart
+++ test/data/repositories/transaction_repository_imp_test.dart
@@ -77,7 +77,6 @@ void main() {
       expect(result, isNotNull);
       expect(result!.id, testTransaction.id);
       verify(mockDataSource.getById(testTransaction.id!)).called(1);
-
     });
 
   });
