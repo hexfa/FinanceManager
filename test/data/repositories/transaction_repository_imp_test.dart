2c1f573f5d86b48bce7a907c979df412a486671d
@hexfa <amir.dehdarian2009@gmail.com>
2026-05-06 00:27:18 +0300

    Clean code

--- test/data/repositories/transaction_repository_imp_test.dart
+++ test/data/repositories/transaction_repository_imp_test.dart
@@ -21,7 +21,6 @@ void main() {
     category: Category(id: 1, name: 'sport'),
     date: DateTime(2023, 1, 1),
   );
-
   final testTransactionEntity = testTransaction.toEntity();
   setUp(() {
     mockDataSource = MockDBDataSource();
