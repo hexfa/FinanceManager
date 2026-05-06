81b9757ebd36481f5ffc8330cd06ee1e9361df42
@hexfa <amir.dehdarian2009@gmail.com>
2026-05-06 15:13:36 +0300

    Clean code

--- test/helpers/build_testable_widget.dart
+++ test/helpers/build_testable_widget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:finance_manager/presentation/bloc/setting/setting_cubit.dart';
-
 import 'setting_cubit_fake.dart';
 
 
