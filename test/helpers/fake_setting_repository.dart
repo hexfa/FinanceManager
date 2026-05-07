3544816c4312ed1e6ed7fbd4a7a439892e9a9e05
@hexfa <amir.dehdarian2009@gmail.com>
2026-05-07 16:49:17 +0300

    Test

--- test/helpers/fake_setting_repository.dart
+++ test/helpers/fake_setting_repository.dart
@@ -1,33 +1,67 @@
+
 import 'package:finance_manager/domain/repositories/setting_repository.dart';
 
+
 class FakeSettingRepository implements SettingRepository {
+
   String _currencyCode;
+
   bool _darkTheme;
 
+
+
   FakeSettingRepository({
+
     String initialCurrencyCode = 'USD',
+
     bool initialDarkTheme = false,
+
   }) : _currencyCode = initialCurrencyCode,
+
        _darkTheme = initialDarkTheme;
 
+
+
   String? lastSetCurrencyCode;
+
   bool? lastSetDarkTheme;
 
+
+
   @override
+
   String getCurrency() => _currencyCode;
 
+
+
   @override
+
   bool isDarkTheme() => _darkTheme;
 
-  @override
-  Future<void> setCurrency(String code) async {
-    lastSetCurrencyCode = code;
-    _currencyCode = code;
-  }
+
 
   @override
-  Future<void> setDarkTheme(bool value) async {
-    lastSetDarkTheme = value;
-    _darkTheme = value;
+
+  Future<void> setCurrency(String code) async {
+
+    lastSetCurrencyCode = code;
+
+    _currencyCode = code;
+
   }
+
+
+
+  @override
+
+  Future<void> setDarkTheme(bool value) async {
+
+    lastSetDarkTheme = value;
+
+    _darkTheme = value;
+
+
+  }
+
 }
+
