88eb6510a537c13b27997f07246b779c8a9e6541
@hexfa <amir.dehdarian2009@gmail.com>
2026-05-11 14:04:03 +0300

    Clean code

--- test/helpers/mock_category_cubit_test.dart
+++ test/helpers/mock_category_cubit_test.dart
@@ -1,4 +1,3 @@
 import 'package:mocktail/mocktail.dart';
 import 'package:finance_manager/presentation/bloc/category/category_cubit.dart';
-
 class MockCategoryCubit extends Mock implements CategoryCubit {}
